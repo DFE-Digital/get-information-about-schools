5164f59f4d681761d22efb3b480e18df6ea4393d
Kris Dyson <kris.dyson@outlook.com>
2016-09-20 16:45:56 +0100

    Added prototype controller/test view

--- Web/Edubase.Web.UI/App_Start/RouteConfig.cs
+++ Web/Edubase.Web.UI/App_Start/RouteConfig.cs
@@ -10,6 +10,12 @@ namespace Edubase.Web.UI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Prototype",
+                url: "prototype/{viewName}",
+                defaults: new { controller = "Prototype", action = "Index", viewName = "TestView" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ Web/Edubase.Web.UI/Controllers/PrototypeController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace Edubase.Web.UI.Controllers
+{
+    public class PrototypeController : Controller
+    {
+        public ActionResult Index(string viewName) => View(viewName);
+    }
+}
