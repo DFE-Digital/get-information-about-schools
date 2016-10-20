d28530e37b5bdbff0a4a093c4e1bf1756b39cc2a
Kris Dyson <kris.dyson@outlook.com>
2016-10-20 14:53:01 +0100

    4727: Download all school data from Edubase

---
+++ Web/Edubase.Web.UI/Controllers/DownloadsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage;
+using System.Configuration;
+
+namespace Edubase.Web.UI.Controllers
+{
+    public class DownloadsController : Controller
+    {
+        // GET: Downloads
+        public ActionResult Index()
+        {
+            var client = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["DataConnectionString"].ConnectionString).CreateCloudBlobClient();
+            var blobs = client.GetContainerReference("public").GetDirectoryReference("zip").ListBlobs().Cast<CloudBlob>();
+            return View(blobs);
+        }
+    }
+}
