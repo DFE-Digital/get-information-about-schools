d9f296b3635153b3dde33ee986cea8ec48d1b5d6
Kris Dyson <kris.dyson@outlook.com>
2016-12-12 14:59:29 +0000

    Bug 6751: Incorrect initial search criteria

---
+++ Web/Edubase.Web.UI/Helpers/QueryStringHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Edubase.Web.UI.Helpers
+{
+    public class QueryStringHelper
+    {
+        public static string ToQueryString(string key, params int[] ids) => string.Join("&", ids.Select(x => $"{key}={x}"));
+    }
+}
