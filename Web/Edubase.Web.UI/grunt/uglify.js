17a2bf277c8122fe523cb9097f1e32d17d74beda
Jon Banner <jbanner552@gmail.com>
2017-03-09 09:10:39 +0000

    8029 added task to allow local dev with vue.js dev version

--- Web/Edubase.Web.UI/grunt/uglify.js
+++ Web/Edubase.Web.UI/grunt/uglify.js
@@ -14,6 +14,33 @@
               'node_modules/govuk_frontend_toolkit/javascripts/*.js',
               'node_modules/govuk_frontend_toolkit/javascripts/govuk/*.js',
               'node_modules/govuk_frontend_toolkit/javascripts/govuk/analytics/*.js',
+              'Assets/Scripts/libs/vue/vue.min.js',
+              'Assets/Scripts/*.js',
+              // 'Scripts/jquery.validate.js',
+              // 'Scripts/jquery.validate.unobtrusive.js',
+              'Assets/Scripts/Behaviours/Collapsible/*.js',
+              'Assets/Scripts/Behaviours/Accordion/*.js',
+              'Assets/Scripts/Behaviours/PartialRequest/*.js',
+              'Assets/Scripts/Behaviours/floatThead/*.js',
+              'Assets/Scripts/Elements/**/*.js',
+              'Assets/Scripts/Views/**/*.js'
+            ],
+            dest: 'public/assets/scripts/application.js'
+        },
+        "bundle-elements-local": { // task to build application.js with the development version of vue.js
+            options: {
+                sourceMap: false,
+                beautify: true,
+                compress: false,
+                mangle: false,
+                preserveComments: 'some' // allow jScript conditional compilation
+            },
+            src: [
+              // couldn't use /**/*.js as govuk/analytics/error-tracking.js causes an error with GOVUK undefined
+              'node_modules/govuk_frontend_toolkit/javascripts/*.js',
+              'node_modules/govuk_frontend_toolkit/javascripts/govuk/*.js',
+              'node_modules/govuk_frontend_toolkit/javascripts/govuk/analytics/*.js',
+              'Assets/Scripts/libs/vue/vue.js',
               'Assets/Scripts/*.js',
               // 'Scripts/jquery.validate.js',
               // 'Scripts/jquery.validate.unobtrusive.js',
