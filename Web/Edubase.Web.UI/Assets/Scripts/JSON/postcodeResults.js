7452fd95a078e5451b07776a2097db4f56772e75
Jon Banner <jbanner552@gmail.com>
2017-03-27 17:49:16 +0100

    9536 address lookup prototype

---
+++ Web/Edubase.Web.UI/Assets/Scripts/JSON/postcodeResults.js
@@ -0,0 +1,306 @@
+﻿[{
+  "query": "S26 4SG",
+  "total": 75,
+  "matches": [
+    {   
+        "id": "0cOGBRCQzgBwAAAAABAwEAAAABJCpSQAAAAAA2OQBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "69 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {     
+        "id": "0COGBRCQzgBwAAAAABAwEAAAABJCpSQAAAAAA3MQBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "71 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {     
+        "id": "0vOGBRCQzgBwAAAAABAwEAAAABJCpSQAAAAAA3MwBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "73 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {      
+        "id": "0lOGBRCQzgBwAAAAABAwEAAAABJCpSQAAAAAA3NQBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "75 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {     
+        "id": "0IOGBRCQzgBwAAAAABAwEAAAABJCpSQAAAAAA3NwBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "77 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {    
+        "id": "04OGBRCQzgBwAAAAABAwEAAAABJCpSQAAAAAA3OQBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "79 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {     
+        "id": "0.OGBRCQzgBwAAAAABAwEAAAABJCpSQAAAAAA4MQBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "81 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {      
+        "id": "0SOGBRCQzgBwAAAAABAwEAAAABJCpSQAAAAAA4MwBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "83 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {     
+        "id": "00OGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAA4NABkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "84 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {     
+        "id": "0hOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAA4NQBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "85 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {      
+        "id": "0ZOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAA4NgBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "86 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {      
+        "id": "0MOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAA4NwBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "87 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {      
+        "id": "0pOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAA4OABkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "88 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "08OGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAA4OQBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "89 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {      
+        "id": "03OGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAA5MABkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "90 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {      
+        "id": "0iOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAA5MQBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "91 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {      
+        "id": "0aOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAA5MgBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "92 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {      
+        "id": "0POGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAA5MwBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "93 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {      
+        "id": "0QOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAA5NABkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "94 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {     
+        "id": "0FOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAA5NQBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "95 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {      
+        "id": "09OGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAA5NgBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "96 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {      
+        "id": "0oOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAA5NwBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "97 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0NOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAA5OABkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "98 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    { 
+        "id": "0YOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAA5OQBkAAAAAP....8AAAAAAAAAAAA-",
+        "name": "99 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {      
+        "id": "0ZOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAAxMDAAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "100 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0HOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAAxMDEAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "101 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    { 
+        "id": "0lOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAAxMDIAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "102 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    { 
+        "id": "07OGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAAxMDMAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "103 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0hOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAAxMDQAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "104 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0.OGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAAxMDUAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "105 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0dOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAAxMDYAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "106 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0HOGBRCQzgBwAAAAABAwEAAAABJCpVgAAAAAAxMDcAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "107 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0eOGBRCQzgBwAAAAABAwEAAAABJCpcwAAAAAAAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "109 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "02OGBRCQzgBwAAAAABAwEAAAABJCpdwAAAAAAxMTEAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "111 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0UOGBRCQzgBwAAAAABAwEAAAABJCpdwAAAAAAxMTIAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "112 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0KOGBRCQzgBwAAAAABAwEAAAABJCpdwAAAAAAxMTMAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "113 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0QOGBRCQzgBwAAAAABAwEAAAABJCpdwAAAAAAxMTQAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "114 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0OOGBRCQzgBwAAAAABAwEAAAABJCpdwAAAAAAxMTUAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "115 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0sOGBRCQzgBwAAAAABAwEAAAABJCpdwAAAAAAxMTYAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "116 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0UOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxMTgAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "118 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0mOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxMjAAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "120 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0aOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxMjIAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "122 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0eOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxMjQAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "124 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0iOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxMjYAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "126 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0mOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxMjgAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "128 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0zOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxMzAAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "130 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0POGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxMzIAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "132 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0LOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxMzQAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "134 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "03OGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxMzYAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "136 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0zOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxMzgAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "138 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0mOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNDAAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "140 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0aOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNDIAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "142 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0eOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNDQAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "144 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0iOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNDYAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "146 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0mOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNDgAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "148 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0zOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNTAAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "150 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0POGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNTIAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "152 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0LOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNTQAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "154 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "03OGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNTYAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "156 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0zOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNTgAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "158 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0BOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNjAAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "160 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "09OGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNjIAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "162 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "05OGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNjQAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "164 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0FOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNjYAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "166 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0BOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNjgAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "168 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0UOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNzAAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "170 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0oOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNzIAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "172 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0sOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNzQAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "174 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0QOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNzYAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "176 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0UOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxNzgAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "178 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "07OGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxODAAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "180 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0HOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxODIAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "182 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0DOGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxODQAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "184 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "0.OGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxODYAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "186 Haigh Moor Way, Swallownest, SHEFFIELD"
+    },
+    {
+        "id": "07OGBRCQzgBwAAAAABAwEAAAABJCpggAAAAAAxODgAZAAAAAD.....AAAAAAAAAAAA",
+        "name": "188 Haigh Moor Way, Swallownest, SHEFFIELD"
+    }
+  ]
+}]
