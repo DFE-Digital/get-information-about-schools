94c9160aae23f11139d6962b16c1559cf289303f
Jon Banner <jbanner552@gmail.com>
2017-04-05 14:36:08 +0100

    10729 tooltips

--- Web/Edubase.Web.UI/Assets/Scripts/tooltips.js
+++ Web/Edubase.Web.UI/Assets/Scripts/tooltips.js
@@ -1,6 +1,6 @@
 ﻿(function($) {
     var defaults = {
-        contentTarget: '#tooltip-content',
+        contentTarget: '#tooltip-content'
 
     }
 
@@ -12,41 +12,87 @@
     }
 
     function tooltipScaffold() {
-        var tooltip = '<div id="edubase-tooltip" class="edubase-tooltip hidden" aria-hidden="true"><span class="pointer"></span>' +
+        var tooltip = '<div id="edubase-tooltip" class="edubase-tooltip hidden" aria-hidden="true">' +
+            '<span class="pointer-shadow"></span><span class="pointer"></span>' +
             '<a href="#" class="tooltip-close" title="close tool tip"><span class="visuallyhidden">Close</span></a>' +
-            '<div id="tooltip-content" class="tooltip-content" tab-index="0"></div></div>';
+            '<div id="tooltip-content" class="tooltip-content" tab-index="-1"></div></div>';
 
-        $('#content').append(tooltip);
+        $('#full-content').after(tooltip);
     }
 
     ToolTip.prototype = {
         init: function () {
-           tooltipScaffold();
-           var el = this.el,
-               opts = this.opts,            
-               $toolTip = $('#edubase-tooltip');
+            if (!document.getElementById('edubase-tooltip')) {
+                tooltipScaffold();
+            }
+           
+            var el = this.el,
+                opts = this.opts,
+                self = this;
+
+            this.$toolTip = $('#edubase-tooltip');
+
+           var closeToolTip = function closeToolTip() {
+                self.$toolTip.addClass('hidden')
+                    .attr('aria-hidden', true);
+
+                $('body').off('click', '#full-content', closeToolTip);
+           }
 
             $(el).on('click', function(e) {
                 e.preventDefault();
                 var contentSelector = $(this).attr('href');
                 var content = $(contentSelector).html();
-                var offSet = $(this).offset();
 
                 $(opts.contentTarget).html(content);
+                
+                self.positionToolTip();
 
-                $toolTip.css({ top: offSet.top + 45 + 'px', left: offSet.left - 26 + 'px' });
-
-                $toolTip.removeClass('hidden')
+                self.$toolTip.removeClass('hidden')
                     .attr('aria-hidden', false);
+
                 $('#tooltip-content').focus();
+
+                window.setTimeout(function() {
+                    $('body').on('click', '#full-content', closeToolTip);
+                }, 20);
+
             });
 
-            $toolTip.find('.tooltip-close').on('click', function(e) {
+            this.$toolTip.find('.tooltip-close').on('click', function(e) {
                 e.preventDefault();
-                $toolTip.addClass('hidden')
-                .attr('aria-hidden', true);
+                closeToolTip();
+                
             });
 
+           
+        },
+        positionToolTip: function() {
+            var el = this.el,
+                opts = this.opts,
+                self = this,
+                winWidth = $(window).width(),
+                offset = $(el).offset(),
+                toolTipWidth = (winWidth < 480) ? 310: 480,
+                leftPos ;
+
+
+            if (offset.left + toolTipWidth > winWidth) {
+                leftPos = winWidth - toolTipWidth - 10;
+
+                self.$toolTip.css({ top: offset.top + 45 + 'px', left: leftPos + 'px' });
+                
+                self.$toolTip.find('.pointer, .pointer-shadow')
+                    .css({
+                        left: -1 * (parseInt(self.$toolTip.css('left'), 10) - offset.left + 7) + 'px'
+                    });
+
+            } else {
+                self.$toolTip.css({ top: offset.top + 45 + 'px', left: offset.left - 26 + 'px' });
+
+                self.$toolTip.find('.pointer, .pointer-shadow').removeAttr('style');
+
+            }
         }
     }
 
@@ -61,3 +107,4 @@
 }($));
 
 $('#content').find('.icon-help').tooltip();
+
