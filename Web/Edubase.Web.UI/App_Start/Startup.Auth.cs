5350aec6e30cfb35c54379ed5154ec45902fef8c
krisdyson <kris.dyson@outlook.com>
2017-04-19 19:10:05 +0100

    10939: Login tex. int. (initial commit)

--- Web/Edubase.Web.UI/App_Start/Startup.Auth.cs
+++ Web/Edubase.Web.UI/App_Start/Startup.Auth.cs
@@ -43,124 +43,39 @@ namespace Edubase.Web.UI
 
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
-            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
-            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
-
-            // Enable the application to use a cookie to store information for the signed in user
-            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
-            // Configure the sign in cookie
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/Account/Login"),
-                Provider = new CookieAuthenticationProvider
-                {
-                    // Enables the application to validate the security stamp when the user logs in.
-                    // This is a security feature which is used when you change a password or add an external login to your account.  
-                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-                        validateInterval: ConfiguredExpireTimeSpan,
-                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
-                },
+                Provider = new CookieAuthenticationProvider(),
                 ExpireTimeSpan = ConfiguredExpireTimeSpan
             });
             
-
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-
             app.UseKentorAuthServicesAuthentication(CreateAuthServicesOptions());
-
-           
             AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
             
         }
-
         private static KentorAuthServicesAuthenticationOptions CreateAuthServicesOptions()
         {
-            var spOptions = CreateSPOptions();
-            var authServicesOptions = new KentorAuthServicesAuthenticationOptions(false)
+            var spOptions = new SPOptions
             {
-                SPOptions = spOptions
+                EntityId = new EntityId(AuthConfig.ApplicationIdpEntityId),
+                ReturnUrl = AuthConfig.ExternalAuthDefaultCallbackUrl
             };
 
-            var idp = new IdentityProvider(new EntityId("http://secure-access-simulator.azurewebsites.net/Metadata"), spOptions)
+            var authServicesOptions = new KentorAuthServicesAuthenticationOptions(false) { SPOptions = spOptions };
+
+            var idp = new IdentityProvider(new EntityId(AuthConfig.ExternalIdpEntityId.AbsoluteUri), spOptions)
             {
                 AllowUnsolicitedAuthnResponse = true,
                 Binding = Saml2BindingType.HttpRedirect,
-                SingleSignOnServiceUrl = new Uri("http://secure-access-simulator.azurewebsites.net/")
+                MetadataLocation = AuthConfig.ExternalIdpMetadataPath
             };
 
-            idp.SigningKeys.AddConfiguredKey(
-                new X509Certificate2(
-                    HostingEnvironment.MapPath(
-                        "~/App_Data/Kentor.AuthServices.StubIdp.cer")));
-
+            idp.SigningKeys.AddConfiguredKey(new X509Certificate2(AuthConfig.ExternalIdpCertificatePath));
             authServicesOptions.IdentityProviders.Add(idp);
-
-            // It's enough to just create the federation and associate it
-            // with the options. The federation will load the metadata and
-            // update the options with any identity providers found.
-            new Federation("http://secure-access-simulator.azurewebsites.net/Federation", true, authServicesOptions);
-
             return authServicesOptions;
         }
-
-        private static SPOptions CreateSPOptions()
-        {
-            var swedish = CultureInfo.GetCultureInfo("sv-se");
-
-            var organization = new Organization();
-            organization.Names.Add(new LocalizedName("Edubase", swedish));
-            organization.DisplayNames.Add(new LocalizedName("Edubase", swedish));
-            organization.Urls.Add(new LocalizedUri(new Uri("http://www.edubase.gov.uk"), swedish));
-
-            var spOptions = new SPOptions
-            {
-                EntityId = new EntityId("http://edubase.gov"),
-                //EntityId = new EntityId("http://localhost:55600/AuthServices"),
-                //ReturnUrl = new Uri("http://localhost:55600/Account/ExternalLoginCallback"),
-                //DiscoveryServiceUrl = new Uri("http://secure-access-simulator.azurewebsites.net/DiscoveryService"),
-                Organization = organization
-            };
-
-            var techContact = new ContactPerson
-            {
-                Type = ContactType.Technical
-            };
-            techContact.EmailAddresses.Add("authservices@example.com");
-            spOptions.Contacts.Add(techContact);
-
-            var supportContact = new ContactPerson
-            {
-                Type = ContactType.Support
-            };
-            supportContact.EmailAddresses.Add("support@example.com");
-            spOptions.Contacts.Add(supportContact);
-
-            var attributeConsumingService = new AttributeConsumingService("AuthServices")
-            {
-                IsDefault = true,
-            };
-
-            attributeConsumingService.RequestedAttributes.Add(
-                new RequestedAttribute("urn:someName")
-                {
-                    FriendlyName = "Some Name",
-                    IsRequired = true,
-                    NameFormat = RequestedAttribute.AttributeNameFormatUri
-                });
-
-            attributeConsumingService.RequestedAttributes.Add(
-                new RequestedAttribute("Minimal"));
-
-            spOptions.AttributeConsumingServices.Add(attributeConsumingService);
-            //spOptions.ServiceCertificates.Add(GetCert());
-            
-            return spOptions;
-        }
-
-        
     }
 }
