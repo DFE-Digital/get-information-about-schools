f18e2a1d8235813f5e5052642245ddc2a7be8b61
Jon Banner <jbanner552@gmail.com>
2017-04-20 14:27:02 +0100

    9438 IEBT proprietor edit

---
+++ Web/Edubase.Web.UI/Assets/Scripts/Elements/radio-toggles.js
@@ -0,0 +1,79 @@
+﻿(function ($) {
+    'use strict';
+    var defaults = {
+        panelDataKey: 'togglePanel',
+        untoggle: false
+    }
+
+    function RadioToggle(el, opts) {
+        this.el = el;
+        this.opts = $.extend({}, defaults, opts);
+
+        this.init();
+    }
+
+    RadioToggle.prototype = {
+        init: function () {
+            var $el = $(this.el),
+                opts = this.opts,
+                $radios = $el.find(':radio'),
+                $panels = [];
+
+            $radios.each(function () {
+                if ($(this).data().hasOwnProperty(opts.panelDataKey)) {
+                    $panels.push($(this).data()[opts.panelDataKey]);
+                }
+            });
+
+
+            $panels = $($panels.join(','));
+
+            $panels.addClass('hidden');
+
+
+            if ($radios.filter(':checked').length > 0) {
+                $($radios.filter(':checked').data()[opts.panelDataKey]).removeClass('hidden');
+            }
+
+
+            $radios.on('change', function () {
+                var radioButton = this;
+                $(window).trigger({
+                    type: 'radioChange',
+                    selectedRadio: radioButton
+                });
+
+                $panels.addClass('hidden')
+                    .attr('aria-hidden', 'true');
+
+                if ($(this).data().hasOwnProperty(opts.panelDataKey)) {
+                    var panelClass = $(this).data()[opts.panelDataKey];
+                    $(panelClass)
+                        .removeClass('hidden')
+                        .attr('aria-hidden', 'false');
+                }
+            });
+
+            if (opts.untoggle) {
+                $(window).on('radioChange', function (e) {
+                    $radios.prop('checked', false);
+                    $panels.addClass('hidden');
+                    $(e.selectedRadio).prop('checked', true);
+                });
+            }
+        }
+    };
+
+
+    $.fn.radioToggle = function (opts) {
+        return this.each(function () {
+            if (!$.data(this, 'radioToggle')) {
+                $.data(this, 'radioToggle', new RadioToggle(this, opts));
+            }
+        });
+    }
+
+
+}($));
+
+$('#content').find('.toggle-panels').radioToggle();
---
+++ Web/Edubase.Web.UI/Assets/Scripts/Views/edit-iebt.js
@@ -0,0 +1,72 @@
+﻿DfE.Views.editIebt = {
+    radios: $('#proprietor-radios').find('input'),
+    cloneFields: $('#cloneable-fields-container').find('.cloneable').detach(),
+    okClick: function() {
+        this.closeModal();
+        $('#proprietor-single, #proprietor-body').find('.form-control').val('');
+        return true;
+    },
+    cancelClick: function() {
+        this.closeModal();
+        var selectedVal = $('#proprietor-radios').find('input:checked').val();
+
+        if (selectedVal === 'single') {
+            $('#proprietor-type-body').prop('checked', true).change();
+
+        } else if (selectedVal === 'body') {
+            $('#proprietor-type-single').prop('checked', true).change();
+        }
+
+    },
+    init: function () {
+        var self = this;
+        var radioValue;
+
+        if (self.radios.length === 0) {
+            return;
+        }
+        
+        radioValue = self.radios.filter(':checked').val();
+
+        if (typeof radioValue === 'undefined') {
+            $('#proprietor-type-single').prop('checked', true);
+            radioValue = self.radios.filter(':checked').val();
+        }
+
+        if (radioValue === 'single') {
+            $('#cloneable-fields-container').append(self.cloneFields);
+        }
+
+        if (radioValue === 'body') {
+            $('#field-clone-target').append(self.cloneFields);
+        }
+
+        $(window).on('radioChange', function (e) {
+            var radioId = $(e.selectedRadio).prop('id');
+
+            if (radioId === 'proprietor-type-body') {
+                $('#field-clone-target').append(self.cloneFields);
+                $('#proprietor-radios').find('input').data().okCancel
+                    .updateModalContent('Are you sure you want to change to a proprietary body?',
+                        'All single proprietor changes will be lost');
+
+               
+            } else {
+                $('#cloneable-fields-container').append(self.cloneFields);
+                 $('#proprietor-radios').find('input').data().okCancel
+                    .updateModalContent('Are you sure you want to change to a single proprietor?',
+                        'All proprietary body changes will be lost');
+            }
+
+        });
+
+        self.radios.okCancel({
+            ok: self.okClick,
+            cancel: self.cancelClick
+        });
+
+    }
+    
+};
+
+DfE.Views.editIebt.init();
