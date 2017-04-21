a52704c7050ed5253262152eca65e54ddedba67c
krisdyson <kris.dyson@outlook.com>
2017-04-21 14:41:21 +0100

    10939: More backward compat.

--- Web/Edubase.Web.UI/App_Start/Startup.Auth.cs
+++ Web/Edubase.Web.UI/App_Start/Startup.Auth.cs
@@ -41,6 +41,8 @@ namespace Edubase.Web.UI
             }
         }
 
+#if (TEXAPI)
+
         public void ConfigureAuth(IAppBuilder app)
         {
             app.UseCookieAuthentication(new CookieAuthenticationOptions
@@ -64,11 +66,10 @@ namespace Edubase.Web.UI
                 ReturnUrl = AuthConfig.ExternalAuthDefaultCallbackUrl
             };
 
-            var cert = new X509Certificate2(HostingEnvironment.MapPath("~/app_data/edubase3.pfx"), "testtest");
             spOptions.ServiceCertificates.Add(new ServiceCertificate
             {
                 Use = CertificateUse.Signing,
-                Certificate = cert
+                Certificate = GetSPCertificate()
             });
 
             var authServicesOptions = new KentorAuthServicesAuthenticationOptions(false) { SPOptions = spOptions };
@@ -85,5 +86,149 @@ namespace Edubase.Web.UI
             authServicesOptions.IdentityProviders.Add(idp);
             return authServicesOptions;
         }
+
+        private static X509Certificate2 GetSPCertificate()
+        {
+#if (DEBUG)
+            return GetSPCertificateFromAppData();
+#else
+            return GetSPCertificateFromCertStore();
+#endif
+        }
+
+        private static X509Certificate2 GetSPCertificateFromAppData() => new X509Certificate2(HostingEnvironment.MapPath("~/app_data/edubase3.pfx"), "testtest");
+
+        private static X509Certificate2 GetSPCertificateFromCertStore()
+        {
+            using (var store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+            {
+                var thumbprint = ConfigurationManager.AppSettings["ServiceProvider.Certificate.Thumbprint"];
+                store.Open(OpenFlags.ReadOnly);
+                var certCollection = store.Certificates.Find(X509FindType.FindByThumbprint, ConfigurationManager.AppSettings["ServiceProvider.Certificate.Thumbprint"], false);
+                var cert = certCollection.Cast<X509Certificate2>().FirstOrDefault();
+                Guard.IsNotNull(cert, () => new Exception($"Service provider certificate could not be found by thumbprint [{thumbprint}]"));
+                return cert;
+            }
+        }
+
+#else
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Configure the db context, user manager and signin manager to use a single instance per request
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
+            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+
+            // Enable the application to use a cookie to store information for the signed in user
+            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
+            // Configure the sign in cookie
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                Provider = new CookieAuthenticationProvider
+                {
+                    // Enables the application to validate the security stamp when the user logs in.
+                    // This is a security feature which is used when you change a password or add an external login to your account.  
+                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                        validateInterval: ConfiguredExpireTimeSpan,
+                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                },
+                ExpireTimeSpan = ConfiguredExpireTimeSpan
+            });
+
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            app.UseKentorAuthServicesAuthentication(CreateAuthServicesOptions());
+
+
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
+
+        }
+
+        private static KentorAuthServicesAuthenticationOptions CreateAuthServicesOptions()
+        {
+            var spOptions = CreateSPOptions();
+            var authServicesOptions = new KentorAuthServicesAuthenticationOptions(false)
+            {
+                SPOptions = spOptions
+            };
+
+            var idp = new IdentityProvider(new EntityId("http://secure-access-simulator.azurewebsites.net/Metadata"), spOptions)
+            {
+                AllowUnsolicitedAuthnResponse = true,
+                Binding = Saml2BindingType.HttpRedirect,
+                SingleSignOnServiceUrl = new Uri("http://secure-access-simulator.azurewebsites.net/")
+            };
+
+            idp.SigningKeys.AddConfiguredKey(
+                new X509Certificate2(
+                    HostingEnvironment.MapPath(
+                        "~/App_Data/Kentor.AuthServices.StubIdp.cer")));
+
+            authServicesOptions.IdentityProviders.Add(idp);
+
+            // It's enough to just create the federation and associate it
+            // with the options. The federation will load the metadata and
+            // update the options with any identity providers found.
+            new Federation("http://secure-access-simulator.azurewebsites.net/Federation", true, authServicesOptions);
+
+            return authServicesOptions;
+        }
+
+        private static SPOptions CreateSPOptions()
+        {
+            var swedish = CultureInfo.GetCultureInfo("sv-se");
+
+            var organization = new Organization();
+            organization.Names.Add(new LocalizedName("Edubase", swedish));
+            organization.DisplayNames.Add(new LocalizedName("Edubase", swedish));
+            organization.Urls.Add(new LocalizedUri(new Uri("http://www.edubase.gov.uk"), swedish));
+
+            var spOptions = new SPOptions
+            {
+                EntityId = new EntityId("http://edubase.gov"),
+                Organization = organization
+            };
+
+            var techContact = new ContactPerson
+            {
+                Type = ContactType.Technical
+            };
+            techContact.EmailAddresses.Add("authservices@example.com");
+            spOptions.Contacts.Add(techContact);
+
+            var supportContact = new ContactPerson
+            {
+                Type = ContactType.Support
+            };
+            supportContact.EmailAddresses.Add("support@example.com");
+            spOptions.Contacts.Add(supportContact);
+
+            var attributeConsumingService = new AttributeConsumingService("AuthServices")
+            {
+                IsDefault = true,
+            };
+
+            attributeConsumingService.RequestedAttributes.Add(
+                new RequestedAttribute("urn:someName")
+                {
+                    FriendlyName = "Some Name",
+                    IsRequired = true,
+                    NameFormat = RequestedAttribute.AttributeNameFormatUri
+                });
+
+            attributeConsumingService.RequestedAttributes.Add(
+                new RequestedAttribute("Minimal"));
+
+            spOptions.AttributeConsumingServices.Add(attributeConsumingService);
+            
+            return spOptions;
+        }
+
+        
+#endif
     }
 }
