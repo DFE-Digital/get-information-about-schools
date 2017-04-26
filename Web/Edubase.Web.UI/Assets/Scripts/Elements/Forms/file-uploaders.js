0e2e90a7b724d3e4e951f0899f7e10072fe710e8
Jon Banner <jbanner552@gmail.com>
2017-04-26 16:33:01 +0100

    9531 establishment bulk update

---
+++ Web/Edubase.Web.UI/Assets/Scripts/Elements/Forms/file-uploaders.js
@@ -0,0 +1,49 @@
+﻿(function($) {
+    'use strict';
+
+    var defaults = {}
+
+    function Uploader(el, opts) {
+        this.el = el;
+        this.opts = $.extend({}, defaults, opts);
+
+        this.init();
+    }
+
+    Uploader.prototype = {
+        init: function() {
+            console.log("init");
+            var $el = $(this.el),
+                opts = this.opts;
+
+
+            var $fileInput = $el.find('input[type="file"]'),
+                $displayBox = $el.find('.file-name'),
+                $button = $el.find('.file-selector');
+
+
+            $fileInput.on('change', function () {
+                var fileName = $(this).val().split('\\').slice(-1);
+                $displayBox.val(fileName);
+            });
+
+
+            $button.on('click', function (e) {
+                e.preventDefault();
+                $fileInput.click();
+            });
+        }
+    }
+
+
+    $.fn.uploadControl = function(opts) {
+        return this.each(function() {
+            if (!$.data(this, 'uploadControl')) {
+                $.data(this, 'uploadControl', new Uploader(this, opts));
+            }
+        });
+    }
+}($));
+
+
+$('#content').find('.upload-control-wrapper').uploadControl();
