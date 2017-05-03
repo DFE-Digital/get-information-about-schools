61889feb41a30d7f3596c7fb263500d0f8dddbfc
Jon Banner <jbanner552@gmail.com>
2017-05-03 14:05:58 +0100

    11387 / 11389 edit governors UI tweaks

--- Web/Edubase.Web.UI/Assets/Scripts/Elements/radio-toggles.js
+++ Web/Edubase.Web.UI/Assets/Scripts/Elements/radio-toggles.js
@@ -1,4 +1,9 @@
-﻿(function ($) {
+﻿// toggle panels from radios or checkboxes
+// radios only allow one panel expanded at a time
+// checkboxes can have mulitple open
+// not currently tested with mixed radios/ checkboxes + don't feed after midnight
+
+; (function ($) {
     'use strict';
     var defaults = {
         panelDataKey: 'togglePanel',
@@ -16,7 +21,7 @@
         init: function () {
             var $el = $(this.el),
                 opts = this.opts,
-                $radios = $el.find(':radio'),
+                $radios = $el.find(':radio, :checkbox'),
                 $panels = [];
 
             $radios.each(function () {
@@ -25,11 +30,9 @@
                 }
             });
 
-
             $panels = $($panels.join(','));
 
             $panels.addClass('hidden');
-
 
             if ($radios.filter(':checked').length > 0) {
                 $($radios.filter(':checked').data()[opts.panelDataKey]).removeClass('hidden');
@@ -38,19 +41,30 @@
 
             $radios.on('change', function () {
                 var radioButton = this;
+                var isRadio = this.getAttribute('type') === 'radio';
+                var panelClass = $(this).data()[opts.panelDataKey];
+
                 $(window).trigger({
                     type: 'radioChange',
                     selectedRadio: radioButton
                 });
 
-                $panels.addClass('hidden')
+                if (isRadio) {
+                    $panels.addClass('hidden')
                     .attr('aria-hidden', 'true');
 
-                if ($(this).data().hasOwnProperty(opts.panelDataKey)) {
-                    var panelClass = $(this).data()[opts.panelDataKey];
                     $(panelClass)
                         .removeClass('hidden')
                         .attr('aria-hidden', 'false');
+                }
+                else {
+                    $(panelClass).toggleClass('hidden');
+
+                    if ($(panelClass).is(':visible')) {
+                        $(panelClass).attr('aria-hidden', 'false');
+                    } else {
+                        $(panelClass).attr('aria-hidden', 'true');
+                    }                        
                 }
             });
 
