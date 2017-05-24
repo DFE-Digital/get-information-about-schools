f52d4d52842e216b9615fdfca31e18522301b046
Richard Pope <richard@simbiosys.co.uk>
2017-05-24 15:28:50 +0100

    Finished making the group changes tab match UED
    Related Work Items: #11236

---
+++ Web/Edubase.Web.UI/Helpers/EnumExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Runtime.Serialization;
+
+namespace Edubase.Web.UI.Helpers
+{
+    public static class EnumExtensions
+    {
+        public static string GetEnumMember(this Enum en)
+        {
+            var type = en.GetType();
+            var memberInfo = type.GetMember(en.ToString());
+            if (memberInfo.Length > 0)
+            {
+                var attributes = memberInfo[0].GetCustomAttributes(typeof(EnumMemberAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    return ((EnumMemberAttribute) attributes[0]).Value;
+                }
+            }
+
+            return en.ToString();
+        }
+    }
+}
