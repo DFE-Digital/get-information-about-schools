64a1504623d43a800ad93951226e157faccde5df
Jon Banner <jbanner552@gmail.com>
2017-06-07 15:48:49 +0100

    12503 restrict data status prompt to defined users

---
+++ Web/Edubase.Web.UI/Controllers/Api/UserRolesController.cs
@@ -0,0 +1,33 @@
+﻿using Edubase.Services.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Edubase.Web.UI.Models;
+
+namespace Edubase.Web.UI.Controllers.Api
+{
+  
+    public class UserRolesController : Controller
+    {
+        // GET: UserRoles
+        [Route("api/user-roles"), HttpGet]
+        public JsonResult Index()
+        {
+            var model = new UserRolesViewModel
+            {
+                UserRequiresDataPrompt = User.InRole(
+                        EdubaseRoles.EFADO,     // EFA data Owners
+                        EdubaseRoles.AP_AOS,    // Academy openers
+                        EdubaseRoles.IEBT,      // Independent schools
+                        EdubaseRoles.APT,       // Alt provision team
+                        EdubaseRoles.SOU,       // SOPT
+                        EdubaseRoles.FST        // Free school openers
+                    )
+            };
+
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
