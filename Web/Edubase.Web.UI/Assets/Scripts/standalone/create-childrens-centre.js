3e46bb9d1e282bf01747d84f47472a6f6a60fbca
Jon Banner <jbanner552@gmail.com>
2017-06-14 13:24:03 +0100

    11370 FED in progress

---
+++ Web/Edubase.Web.UI/Assets/Scripts/standalone/create-childrens-centre.js
@@ -0,0 +1,74 @@
+﻿(function () {
+
+
+    var ccGroup = new Vue({
+        el: '#create-childrens-centre',
+        data: {
+            groupType: '9',
+            groupName: '',
+            groupStatus: 'Open',
+            openDateDay: '',
+            openDateMonth: '',
+            openDateYear: '',
+            la: '',
+
+            step0Complete: false,
+            step1Complete: false,
+
+            openDateValidateUrl: '/Groups/Group/CreateChildrensCentre/Validate/JoinedDate',
+            openDateError: false,
+            openDateErrorMessage: '',
+            urnLookupUrl: ''
+
+
+        },
+        computed: {
+            openDate: function () {
+                if (this.openDateDay !== '' && this.openDateMonth !== '' && this.openDateYear !== '') {
+                    return [this.openDateDay, this.openDateMonth, this.openDateYear].join('/');
+                }
+                return '';
+
+            },
+            laName: function () {
+                return $('#LocalAuthorityId').find('option[value="' + this.la + '"]').text();
+            }
+        },
+        methods: {
+            validateJoinDate: function () {
+                var self = this;
+                self.openDateError = false;
+
+                $.ajax({
+                    url: self.openDateValidateUrl,
+                    method: 'post',
+                    dataType: 'json',
+                    data: {
+                        Day: self.openDateDay,
+                        Month: self.openDateMonth,
+                        Year: self.openDateYear,
+                        Label: ""
+                    },
+                    success: function (data) {
+                        self.openDateError = data.length > 0;
+                        var errors = [];
+                        $.each(data, function (n, err) {
+                            errors.push(err.Value.Errors[0].ErrorMessage);
+                        });
+
+                        self.openDateErrorMessage = errors.join('<br>');
+
+                    }
+                });
+            },
+            step0Continue: function () {
+                this.step0Complete = true;
+            },
+            step1Continue: function () {
+                this.validateJoinDate();
+            }
+
+        }
+    });
+
+}());
