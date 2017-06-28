cfdee2a278ec7b3b980995b46c81da3125f865e0
Richard Pope <richard@simbiosys.co.uk>
2017-06-28 17:17:34 +0100

    Started work for create children's centre

--- Web/Edubase.Web.UI/Models/AddressViewModel.cs
+++ Web/Edubase.Web.UI/Models/AddressViewModel.cs
@@ -1,19 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Edubase.Web.UI.Models
 {
     public class AddressViewModel
     {
+        [Display(Name="Street")]
         public string Line1 { get; set; }
+
+        [Display(Name = "Locality")]
         public string Line2 { get; set; }
+
+        [Display(Name = "Address 3")]
         public string Line3 { get; set; }
+
+        [Display(Name = "Town")]
         public string CityOrTown { get; set; }
+
+        [Display(Name = "County")]
         public string County { get; set; }
+
+        [Display(Name = "Country")]
         public string Country { get; set; }
-        public string Locality { get; set; }
+
+        [Display(Name = "Postcode")]
         public string PostCode { get; set; }
     }
 }
---
+++ Web/Edubase.Web.UI/Models/Establishments/CreateChildrensCentreViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Edubase.Web.UI.Models.Establishments
+{
+    public class CreateChildrensCentreViewModel : CreateEstablishmentViewModel
+    {
+        [Display(Name = "Open date (required tp save record)")]
+        public DateTimeViewModel OpenDate { get; set; }
+
+        [Display(Name= "Address details")]
+        public AddressViewModel Address { get; set; }
+
+        [Display(Name = "Manager first name (required to save record)")]
+        public string ManagerFirstName { get; set; }
+
+        [Display(Name = "Manager last name (required to save record)")]
+        public string ManagerLastName { get; set; }
+
+        [Display(Name = "Manager email (required to save record)")]
+        public string ManagerEmail { get; set; }
+
+        [Display(Name = "Telephone (required to save record)")]
+        public string Telephone { get; set; }
+    }
+}
