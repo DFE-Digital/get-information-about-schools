0fe8cc1573debcf2d25a6a68de23196ede230301
Jon Banner <jbanner552@gmail.com>
2017-07-07 18:29:22 +0100

    13452 create childrens centre validation via api

--- Web/Edubase.Web.UI/Assets/Scripts/standalone/create-childrens-centre.js
+++ Web/Edubase.Web.UI/Assets/Scripts/standalone/create-childrens-centre.js
@@ -35,6 +35,8 @@
             joinDateErrorMessage: '',
 
             urnLookupUrl: '/api/establishment/{0}',
+            validateUrl: '/Groups/Group/CreateChildrensCentre/Validate/All',
+            apiErrors: [],
             urnError: false,
             appState: 'initial', // initial || groupDetail || addCentre || detail
             pendingEdit: false,
@@ -48,6 +50,12 @@
                     return 'group';
                 }
                 return 'collaboration';
+            },
+            wasWere: function() {
+                if (this.apiErrors.length > 1) {
+                    return ' were ';
+                }
+                return ' was an ';
             },
             openDate: function () {
                 if (this.openDateDay !== '' && this.openDateMonth !== '' && this.openDateYear !== '') {
@@ -102,6 +110,8 @@
             submitCentres: function () {
                 var self = this;
                 var centres = this.centresInGroup;
+                var len = centres.length;
+                var i = 0;
                 var frag = document.createDocumentFragment();
                 var form = document.getElementById('create-childrens-centres');
                 var fields = [
@@ -111,39 +121,101 @@
                     'JoinedDateEditable.Year',
                     'CCIsLeadCentre'
                 ];
+                var estabs = [];
 
-                for (var i = 0, len = centres.length; i < len; i++) {
-                    var centre = centres[i];
-                    for (var j = 0, fLen = fields.length; j < fLen; j ++) {
-                        var input = document.createElement('input');
-                        input.type = 'hidden';
-                        input.name = 'LinkedEstablishments.Establishments[' + i + '].' + fields[j];
+                this.isProcessing = true;
 
-                        switch(j) {
-                            case 0:
-                                input.value = centre.urn;
-                                break;
-                            case 1:
-                                input.value = centre.joinDate.split('/')[0];
-                                break;
-                            case 2:
-                                input.value = centre.joinDate.split('/')[1];
-                                break;
-                            case 3:
-                                input.value = centre.joinDate.split('/')[2];
-                                break;
-                            case 4:
-                                input.value = centre.urn === self.groupLead;
-                                break;
-                        }
-                        frag.appendChild(input);
-                    }
+                function formatComDate(date) {
+                    var d = date.split('/');
+                    return [d[2], d[1], d[0]].join('-');
                 }
-                form.appendChild(frag);
-                window.setTimeout(function() {
-                    form.submit();
-                },100);
                 
+                for (i = 0; i < len; i++) {
+                    var o = {};
+                    var centre = self.centresInGroup[i];
+                    o.urn = centre.urn;
+                    o.joinedDate = formatComDate(centre.joinDate);
+                    o.cCIsLeadCentre = centre.urn === self.groupLead;
+                    estabs.push(o);
+                }
+
+                var validationObj = {
+                    "groupTypeId": self.groupType,
+                    "name": self.groupName,
+                    "openDate": [self.openDateYear, self.openDateMonth, self.openDateDay].join('-'),
+                    "localAuthorityId": self.la,
+                    "establishments": estabs
+                }
+
+                self.apiErrors = [];
+                function validate() {
+                    return $.ajax({
+                        url: self.validateUrl,
+                        method: 'post',
+                        contentType: 'application/json; charset=utf-8',
+                        dataType: 'json',
+                        data: JSON.stringify(validationObj),
+                        success: function (data) {
+                            if (data.length > 0) {
+                                $(data).each(function (n, error) {
+                                    var o = {};
+                                    /// if error.Fields contains a digit, it's a problem with an estab -> look up URN
+                                    if (/\d/.test(error.Fields)) {
+                                        var est = self.centresInGroup[error.Fields.replace(/\D/g, '')];
+                                        o.field = est.name + ' (' + est.urn + ')';
+                                    } else {
+                                        o.field = error.Fields;
+                                    }
+                                    o.message = error.Message;
+                                    self.apiErrors.push(o);
+                                });
+                            }
+                        },
+                        error: function (jqXhr) {
+                            console.log(jqXhr);
+                        }
+                    });
+                }
+
+                $.when(validate()).done(function () {
+                    self.isProcessing = false;
+                    if (self.apiErrors.length === 0) {
+                        for (i = 0; i < len; i++) {
+                            var centre = centres[i];
+                            for (var j = 0, fLen = fields.length; j < fLen; j++) {
+                                var input = document.createElement('input');
+                                input.type = 'hidden';
+                                input.name = 'LinkedEstablishments.Establishments[' + i + '].' + fields[j];
+
+                                switch (j) {
+                                    case 0:
+                                        input.value = centre.urn;
+                                        break;
+                                    case 1:
+                                        input.value = centre.joinDate.split('/')[0];
+                                        break;
+                                    case 2:
+                                        input.value = centre.joinDate.split('/')[1];
+                                        break;
+                                    case 3:
+                                        input.value = centre.joinDate.split('/')[2];
+                                        break;
+                                    case 4:
+                                        input.value = centre.urn === self.groupLead;
+                                        break;
+                                }
+                                frag.appendChild(input);
+                            }
+                        }
+
+
+                        form.appendChild(frag);
+
+                        window.setTimeout(function () {
+                            form.submit();
+                        }, 100);
+                    }
+                });
             },
             capitalise: function(str) {
                 return str.charAt(0).toUpperCase() + str.slice(1);
@@ -182,6 +254,7 @@
             lookupUrn: function () {
                 var self = this;
                 this.urnError = false;
+                this.apiErrors = [];
                 if (isNaN(this.searchUrn) || this.searchUrn === '') {
                     this.urnError = true;
                     return;
@@ -230,6 +303,7 @@
             editJoiningEstab: function (urn) {
                 this.searchUrn = urn;
                 this.pendingEdit = true;
+                this.apiErrors = [];
                 this.pendingEstab = this.centresInGroup.filter(function (estab) {
                     if (urn === estab.urn) {
                         return estab;
@@ -248,6 +322,7 @@
                 this.appState = 'detail';
             },
             removeJoiningEstab: function (urn) {
+                this.apiErrors = [];
                 var pos = this.centresInGroup.map(function (estab) {
                     return estab.urn;
                 }).indexOf(urn);
