1ab5f6bfa788d9c439dc618ad27dbddbe3a0fc08
Richard Pope <richard@simbiosys.co.uk>
2017-08-09 09:42:58 +0100

    more changes to re-enable Shared Governors

--- Web/Edubase.Web.UI/Areas/Governors/Controllers/GovernorController.cs
+++ Web/Edubase.Web.UI/Areas/Governors/Controllers/GovernorController.cs
@@ -401,7 +401,6 @@ namespace Edubase.Web.UI.Areas.Governors.Controllers
         [HttpPost, Route(ESTAB_REPLACE_GOVERNOR)]
         public async Task<ActionResult> ReplaceChair(ReplaceChairViewModel model)
         {
-            model.NewChairType = ReplaceChairViewModel.ChairType.LocalChair;
             if (ModelState.IsValid)
             {
                 if (model.NewChairType == ReplaceChairViewModel.ChairType.SharedChair)
