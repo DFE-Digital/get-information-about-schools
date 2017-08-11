ff5e5e37804bc2564f58e8fe5533e09269d388cd
krisdyson <kris.dyson@outlook.com>
2017-08-11 13:40:09 +0100

    APIs for landing page redirect

--- Web/Edubase.Services.Texuna/Security/SecurityApiService.cs
+++ Web/Edubase.Services.Texuna/Security/SecurityApiService.cs
@@ -2,6 +2,7 @@
 using Edubase.Services.Security;
 using System.Security.Principal;
 using System.Threading.Tasks;
+using System;
 
 namespace Edubase.Services.Texuna.Security
 {
@@ -14,11 +15,17 @@ namespace Edubase.Services.Texuna.Security
         public IPrincipal CreateAnonymousPrincipal() => new GenericPrincipal(new GenericIdentity("ANON"), new string[0]);
         
         public async Task<CreateGroupPermissionDto> GetCreateGroupPermissionAsync(IPrincipal principal)
-            => (await _httpClient.GetAsync<CreateGroupPermissionDto>($"security/group/create-permission", principal)).Response;
+            => (await _httpClient.GetAsync<CreateGroupPermissionDto>($"security/group/create-permission", principal)).GetResponse();
         
         public async Task<CreateEstablishmentPermissionDto> GetCreateEstablishmentPermissionAsync(IPrincipal principal)
-            => (await _httpClient.GetAsync<CreateEstablishmentPermissionDto>($"establishment/cancreate", principal)).Response;
+            => (await _httpClient.GetAsync<CreateEstablishmentPermissionDto>($"establishment/cancreate", principal)).GetResponse();
 
-        public async Task<string[]> GetRolesAsync(IPrincipal principal) => (await _httpClient.GetAsync<string[]>($"security/role-names", principal)).Response;
+        public async Task<string[]> GetRolesAsync(IPrincipal principal) => (await _httpClient.GetAsync<string[]>($"security/role-names", principal)).GetResponse();
+
+        public async Task<int?> GetMyEstablishmentUrn(IPrincipal principal)
+            => (await _httpClient.GetAsync<int?>($"user/establishment/urn", principal)).GetResponse();
+
+        public async Task<int?> GetMyMATUId(IPrincipal principal)
+            => (await _httpClient.GetAsync<int?>($"user/group/uid", principal)).GetResponse();
     }
 }
--- Web/Edubase.Web.UI/Controllers/AccountController.cs
+++ Web/Edubase.Web.UI/Controllers/AccountController.cs
@@ -1,18 +1,16 @@
 ﻿using Edubase.Common;
 using Edubase.Services.Security;
 using Edubase.Services.Security.ClaimsIdentityConverters;
-using Edubase.Web.UI.Helpers;
 using Edubase.Web.UI.MvcResult;
-using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System;
+using Edubase.Services.Enums;
 
 namespace Edubase.Web.UI.Controllers
 {
@@ -45,13 +43,33 @@ namespace Edubase.Web.UI.Controllers
             if (ConfigurationManager.AppSettings["owin:appStartup"] == "SASimulatorConfiguration") id = new StubClaimsIdConverter().Convert(id);
             else id = new SecureAccessClaimsIdConverter().Convert(id);
 
-            var roles = await _securityService.GetRolesAsync(new ClaimsPrincipal(id));
+            var principal = new ClaimsPrincipal(id);
+            var roles = await _securityService.GetRolesAsync(principal);
             id.AddClaims(roles.Select(x => new Claim(ClaimTypes.Role, x)));
             
             AuthenticationManager.SignIn(id);
 
-            var urlHelper = new UrlHelper(Request.RequestContext);
-            if (urlHelper.IsLocalUrl(returnUrl)) return Redirect(returnUrl);
+            return GetLandingPage(principal);
+        }
+
+        private ActionResult GetLandingPage(ClaimsPrincipal principal)
+        {
+            if (principal.IsInRole(EdubaseRoles.ESTABLISHMENT))
+            {
+                var urn = _securityService.GetMyEstablishmentUrn(principal);
+                return RedirectToRoute("EstabDetails", new { id = urn });
+            }
+            else if (principal.IsInRole(EdubaseRoles.EDUBASE_GROUP_MAT))
+            {
+                var uid = _securityService.GetMyMATUId(principal);
+                return RedirectToRoute("GroupDetails", new { id = uid });
+            }
+            else if (principal.IsInRole(EdubaseRoles.IEBT))
+            {
+                var types = new[]{eLookupEstablishmentType.NonmaintainedSpecialSchool
+
+                return Redirect("/Establishments/Search?a=31&a=5&a=10&a=7");
+            }
             else return RedirectToAction("Index", "Search");
         }
 
