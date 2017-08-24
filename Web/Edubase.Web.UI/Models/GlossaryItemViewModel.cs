69f641f5c9da51a2b7ba635267af4eac55cc4b32
krisdyson <kris.dyson@outlook.com>
2017-08-24 13:51:43 +0100

    13970: Bits and bobs

--- Web/Edubase.Web.UI/Models/GlossaryItemViewModel.cs
+++ Web/Edubase.Web.UI/Models/GlossaryItemViewModel.cs
@@ -1,10 +1,5 @@
-﻿using Edubase.Data.Entity;
-using Edubase.Data.Repositories.TableStorage;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace Edubase.Web.UI.Models
 {
@@ -15,7 +10,7 @@ namespace Edubase.Web.UI.Models
         [Required]
         public string Title { get; set; }
 
-        [Required, MaxLength(1000, ErrorMessage = "the Content field cannot have more than 1000 characters")]
+        [Required, MaxLength(1000, ErrorMessage = "the Content field cannot have more than 1000 characters"), AllowHtml]
         public string Content { get; set; }
 
         public bool IsDeleting { get; set; }
