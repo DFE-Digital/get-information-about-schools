6168defedfda560c57818122332fe4a99b82d3c6
Richard Pope <richard@simbiosys.co.uk>
2017-08-25 13:55:00 +0100

    WIP

---
+++ Web/Edubase.Web.UI/Areas/Governors/Models/HistoricGovernorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Edubase.Web.UI.Areas.Governors.Models
+{
+    public class HistoricGovernorViewModel : GovernorViewModel
+    {
+        public string FullName { get; set; }
+        public string AppointingBody { get; set; }
+        public string RoleName { get; set; }
+    }
+}
