0f743d4801d62b92ad155d009d125ac432d088a6
Richard Pope <richard@simbiosys.co.uk>
2017-09-07 10:30:55 +0100

    Created base class for unit tests

---
+++ Web/Edubase.UnitTest/Controllers/EstablishmentControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Edubase.Services.Establishments;
+using Edubase.Services.Groups;
+using Edubase.Services.Lookup;
+using Edubase.Services.Security;
+using Edubase.Web.Resources;
+using Edubase.Web.UI.Areas.Establishments.Controllers;
+using Moq;
+using NUnit.Framework;
+
+namespace Edubase.UnitTest.Controllers
+{
+    [TestFixture]
+    public class EstablishmentControllerTest : UnitTestBase<EstablishmentController>
+    {
+        private Mock<IEstablishmentReadService> establishmentReadServiceMock;
+        private Mock<IGroupReadService> groupReadServiceMock;
+        private Mock<IMapper> mapperMock;
+        private Mock<IEstablishmentWriteService> establishmentWriteServiceMock;
+        private Mock<ICachedLookupService> cachedLookupServiceMock;
+        private Mock<IResourcesHelper> resourceHelperMock;
+        private Mock<ISecurityService> securityServiceMock;
+
+        public EstablishmentControllerTest()
+        {
+            ObjectUnderTest = new EstablishmentController(AddMock<IEstablishmentReadService>().Object,
+                AddMock<IGroupReadService>().Object, 
+                AddMock<IMapper>().Object,
+                AddMock<IEstablishmentWriteService>().Object, 
+                AddMock<ICachedLookupService>().Object,
+                AddMock<IResourcesHelper>().Object, 
+                AddMock<ISecurityService>().Object);
+
+        }
+
+        public async Task Estab_EditDetails_Null_Id()
+        {
+            
+        }
+    }
+}
---
+++ Web/Edubase.UnitTest/UnitTestBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Moq;
+
+namespace Edubase.UnitTest
+{
+    public abstract class UnitTestBase<T> where T : class
+    {
+        private readonly List<Mock> mocks = new List<Mock>();
+
+        internal T ObjectUnderTest { get; private set; }
+        internal AutofacDependencyResolver DependencyResolver { get; private set; }
+
+        internal Mock<TMock> AddMock<TMock>() where TMock: class
+        {
+            var mock = new Mock<TMock>(MockBehavior.Strict);
+            mocks.Add(mock);
+            return mock;
+        }
+
+        internal Mock<TMock> GetMock<TMock>() where TMock : Mock
+        {
+            return mocks.SingleOrDefault(m => m is Mock<TMock>) as Mock<TMock>;
+        }
+
+        internal void SetupObjectUnderTest()
+        {
+            var containerBuilder = new ContainerBuilder();
+            foreach (var mock in mocks)
+            {
+                containerBuilder.RegisterInstance(mock.Object).AsImplementedInterfaces().AsSelf();
+            }
+            
+            DependencyResolver = new AutofacDependencyResolver(containerBuilder.Build());
+            ObjectUnderTest = DependencyResolver.GetService(typeof(T)) as T;
+        }
+    }
+}
