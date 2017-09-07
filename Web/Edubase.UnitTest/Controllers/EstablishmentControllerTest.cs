24f3e361f7da0fa8784a3798eee4413d1d836e9d
Richard Pope <richard@simbiosys.co.uk>
2017-09-07 16:30:46 +0100

    Started adding unit tests for EstablishmentController

--- Web/Edubase.UnitTest/Controllers/EstablishmentControllerTest.cs
+++ Web/Edubase.UnitTest/Controllers/EstablishmentControllerTest.cs
@@ -1,6 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 using AutoMapper;
+using Edubase.Services.Domain;
+using Edubase.Services.Enums;
 using Edubase.Services.Establishments;
+using Edubase.Services.Establishments.DisplayPolicies;
+using Edubase.Services.Establishments.Models;
+using Edubase.Services.Exceptions;
 using Edubase.Services.Groups;
 using Edubase.Services.Lookup;
 using Edubase.Services.Security;
@@ -14,29 +23,178 @@ namespace Edubase.UnitTest.Controllers
     [TestFixture]
     public class EstablishmentControllerTest : UnitTestBase<EstablishmentController>
     {
-        private Mock<IEstablishmentReadService> establishmentReadServiceMock;
-        private Mock<IGroupReadService> groupReadServiceMock;
-        private Mock<IMapper> mapperMock;
-        private Mock<IEstablishmentWriteService> establishmentWriteServiceMock;
-        private Mock<ICachedLookupService> cachedLookupServiceMock;
-        private Mock<IResourcesHelper> resourceHelperMock;
-        private Mock<ISecurityService> securityServiceMock;
-
         public EstablishmentControllerTest()
         {
-            ObjectUnderTest = new EstablishmentController(AddMock<IEstablishmentReadService>().Object,
-                AddMock<IGroupReadService>().Object, 
-                AddMock<IMapper>().Object,
-                AddMock<IEstablishmentWriteService>().Object, 
-                AddMock<ICachedLookupService>().Object,
-                AddMock<IResourcesHelper>().Object, 
-                AddMock<ISecurityService>().Object);
+            AddMock<IEstablishmentReadService>();
+            AddMock<IGroupReadService>();
+            AddMock<IMapper>();
+            AddMock<IEstablishmentWriteService>();
+            AddMock<ICachedLookupService>();
+            AddMock<IResourcesHelper>();
+            AddMock<ISecurityService>();
 
+            SetupObjectUnderTest();
         }
 
+        [Test]
         public async Task Estab_EditDetails_Null_Id()
         {
+            var response = await ObjectUnderTest.EditDetails(null, null);
+
+            Assert.IsTrue(response is HttpNotFoundResult);
+        }
+
+        [Test]
+        public async Task Estab_EditDetails_Id_NotFound()
+        {
+            var expectedExceptionThrown = false;
+            GetMock<IEstablishmentReadService>().Setup(e => e.GetAsync(It.IsAny<int>(), It.IsAny<IPrincipal>())).ReturnsAsync(() => new ServiceResultDto<EstablishmentModel>(eServiceResultStatus.NotFound));
+
+            try
+            {
+                await ObjectUnderTest.EditDetails(4, null);
+            }
+            catch (Exception e)
+            {
+                expectedExceptionThrown = ExceptionContains<EntityNotFoundException>(e);
+            }
             
+            Assert.IsTrue(expectedExceptionThrown, "Expected exception of type EntityNotFoundException");
+        }
+
+        [Test]
+        public async Task Estab_Edit_Helpdesk_Null_Id()
+        {
+            var response = await ObjectUnderTest.EditHelpdesk(null as int?);
+
+            Assert.IsTrue(response is HttpNotFoundResult);
+        }
+
+        [Test]
+        public async Task Estab_Edit_Helpdesk_Id_NotFound()
+        {
+            var expectedExceptionThrown = false;
+            GetMock<IEstablishmentReadService>().Setup(e => e.GetAsync(It.IsAny<int>(), It.IsAny<IPrincipal>())).ReturnsAsync(() => new ServiceResultDto<EstablishmentModel>(eServiceResultStatus.NotFound));
+
+            try
+            {
+                await ObjectUnderTest.EditHelpdesk(4);
+            }
+            catch (Exception e)
+            {
+                expectedExceptionThrown = ExceptionContains<EntityNotFoundException>(e);
+            }
+
+            Assert.IsTrue(expectedExceptionThrown, "Expected exception of type EntityNotFoundException");
+        }
+
+        [Test]
+        public async Task Estab_Edit_Location_Null_Id()
+        {
+            var response = await ObjectUnderTest.EditLocation(null as int?);
+
+            Assert.IsTrue(response is HttpNotFoundResult);
+        }
+
+        [Test]
+        public async Task Estab_Edit_Location_Id_NotFound()
+        {
+            var expectedExceptionThrown = false;
+            GetMock<IEstablishmentReadService>().Setup(e => e.GetAsync(It.IsAny<int>(), It.IsAny<IPrincipal>())).ReturnsAsync(() => new ServiceResultDto<EstablishmentModel>(eServiceResultStatus.NotFound));
+
+            try
+            {
+                await ObjectUnderTest.EditLocation(4);
+            }
+            catch (Exception e)
+            {
+                expectedExceptionThrown = ExceptionContains<EntityNotFoundException>(e);
+            }
+
+            Assert.IsTrue(expectedExceptionThrown, "Expected exception of type EntityNotFoundException");
+        }
+
+        [Test]
+        public async Task Estab_Edit_IEBT_Null_Id()
+        {
+            var response = await ObjectUnderTest.EditIEBT(null as int?);
+
+            Assert.IsTrue(response is HttpNotFoundResult);
+        }
+
+        [Test]
+        public async Task Estab_Edit_IEBT_Id_NotFound()
+        {
+            var expectedExceptionThrown = false;
+            GetMock<IEstablishmentReadService>().Setup(e => e.GetAsync(It.IsAny<int>(), It.IsAny<IPrincipal>())).ReturnsAsync(() => new ServiceResultDto<EstablishmentModel>(eServiceResultStatus.NotFound));
+
+            try
+            {
+                await ObjectUnderTest.EditIEBT(4);
+            }
+            catch (Exception e)
+            {
+                expectedExceptionThrown = ExceptionContains<EntityNotFoundException>(e);
+            }
+
+            Assert.IsTrue(expectedExceptionThrown, "Expected exception of type EntityNotFoundException");
+        }
+
+        [Test]
+        public async Task Estab_SearchForEstablishment_Null_Id()
+        {
+            var response = await ObjectUnderTest.SearchForEstablishment(null, null);
+
+            Assert.IsTrue(response is HttpNotFoundResult);
+        }
+
+        [Test]
+        public async Task Estab_SearchForEstablishment_Id_NotFound()
+        {
+            var expectedExceptionThrown = false;
+            GetMock<IEstablishmentReadService>().Setup(e => e.GetAsync(It.IsAny<int>(), It.IsAny<IPrincipal>())).ReturnsAsync(() => new ServiceResultDto<EstablishmentModel>(eServiceResultStatus.NotFound));
+
+            try
+            {
+                await ObjectUnderTest.SearchForEstablishment(4, null);
+            }
+            catch (Exception e)
+            {
+                expectedExceptionThrown = ExceptionContains<EntityNotFoundException>(e);
+            }
+
+            Assert.IsTrue(expectedExceptionThrown, "Expected exception of type EntityNotFoundException");
+        }
+
+        [Test]
+        public async Task Estab_ReplaceEstablishmentAddress()
+        {
+            var nationalities = new List<LookupDto>
+            {
+                new LookupDto {Code = "1", Id = 1, Name = "Nationality 1"},
+                new LookupDto {Code = "2", Id = 2, Name = "Nationality 2"},
+                new LookupDto {Code = "3", Id = 3, Name = "Nationality 3"}
+            };
+
+            var counties = new List<LookupDto>
+            {
+                new LookupDto {Code = "1", Id = 1, Name = "County 1"},
+                new LookupDto {Code = "2", Id = 2, Name = "County 2"},
+                new LookupDto {Code = "3", Id = 3, Name = "County 3"}
+            };
+
+            var establishment = new EstablishmentModel
+            {
+                Urn = 5,
+                Name = "test"
+            };
+
+            GetMock<ICachedLookupService>().Setup(c => c.NationalitiesGetAllAsync()).ReturnsAsync(() => nationalities);
+            GetMock<ICachedLookupService>().Setup(c => c.CountiesGetAllAsync()).ReturnsAsync(() => counties);
+            GetMock<IEstablishmentReadService>().Setup(e => e.GetAsync(It.IsAny<int>(), It.IsAny<IPrincipal>())).ReturnsAsync(() => new ServiceResultDto<EstablishmentModel>(establishment));
+            GetMock<IEstablishmentReadService>().Setup(e => e.GetEditPolicyAsync(It.IsAny<EstablishmentModel>(), It.IsAny<IPrincipal>())).ReturnsAsync(() => new EstablishmentDisplayEditPolicy { IEBTDetail = new IEBTDetailDisplayEditPolicy { AccommodationChangedId = true } });
+
+            await ObjectUnderTest.ReplaceEstablishmentAddressAsync(5, "test");
         }
     }
 }
--- Web/Edubase.UnitTest/UnitTestBase.cs
+++ Web/Edubase.UnitTest/UnitTestBase.cs
@@ -1,18 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Autofac;
-using Autofac.Integration.Mvc;
 using Moq;
 
 namespace Edubase.UnitTest
 {
-    public abstract class UnitTestBase<T> where T : class
+    public abstract class UnitTestBase<T> : IDisposable where T : class
     {
         private readonly List<Mock> mocks = new List<Mock>();
+        private ILifetimeScope containerLifetime;
 
         internal T ObjectUnderTest { get; private set; }
-        internal AutofacDependencyResolver DependencyResolver { get; private set; }
-
+        
         internal Mock<TMock> AddMock<TMock>() where TMock: class
         {
             var mock = new Mock<TMock>(MockBehavior.Strict);
@@ -20,7 +20,7 @@ namespace Edubase.UnitTest
             return mock;
         }
 
-        internal Mock<TMock> GetMock<TMock>() where TMock : Mock
+        internal Mock<TMock> GetMock<TMock>() where TMock : class
         {
             return mocks.SingleOrDefault(m => m is Mock<TMock>) as Mock<TMock>;
         }
@@ -28,13 +28,36 @@ namespace Edubase.UnitTest
         internal void SetupObjectUnderTest()
         {
             var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterType<T>().AsImplementedInterfaces().AsSelf();
+
             foreach (var mock in mocks)
             {
                 containerBuilder.RegisterInstance(mock.Object).AsImplementedInterfaces().AsSelf();
             }
-            
-            DependencyResolver = new AutofacDependencyResolver(containerBuilder.Build());
-            ObjectUnderTest = DependencyResolver.GetService(typeof(T)) as T;
+
+            var container = containerBuilder.Build();
+            containerLifetime = container.BeginLifetimeScope();
+            ObjectUnderTest = containerLifetime.Resolve<T>();
+        }
+
+        internal bool ExceptionContains<TException>(Exception exception) where TException : Exception
+        {
+            while (exception != null)
+            {
+                if (exception is TException)
+                {
+                    return true;
+                }
+
+                exception = exception.InnerException;
+            }
+
+            return false;
+        }
+
+        public void Dispose()
+        {
+            containerLifetime?.Dispose();
         }
     }
 }
