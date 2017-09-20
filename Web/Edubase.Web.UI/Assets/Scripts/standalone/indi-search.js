20f452537effb20efd6305622e3c0ca86444ac14
Jon Banner <jonbanner@outlook.com>
2017-09-20 17:16:48 +0100

    7967 independent schools by date

--- Web/Edubase.Web.UI/Assets/Scripts/standalone/indi-search.js
+++ Web/Edubase.Web.UI/Assets/Scripts/standalone/indi-search.js
@@ -1,25 +1,64 @@
 ﻿(function() {
 
     var filterForm = $('#indi-filter-form');
+    var clearLinks = filterForm.find('.filter-clear');
     var resultsContainer = $('#results-container');
     var filterIntent;
     var downloadLink = $('.download-link');
     var downloadBaseUrl = '/independent-schools/download?';
+    var plsWait = '<div class="progress-indicator"><span class="visually-hidden">Please wait</span></div>';
+    var searchParams;
 
-    var getResults = function() {
+    $('#filter-submit').addClass('hidden');
+
+    var getResults = function () {
+        filterForm.find('input').prop('disabled', 'disabled');
+        clearLinks.addClass('clear-disabled');
+        if (GOVUK.support.history()) {
+            history.pushState({}, null, window.location.href.split('?')[0] + '?' + searchParams);
+        }
         $.ajax({
             url: '/independent-schools/results-js',
-            data: filterForm.serialize(),
+            data: searchParams,
             success: function (data) {
                 resultsContainer.html(data);
-                downloadLink.attr('href', downloadBaseUrl + filterForm.serialize());
+                downloadLink.attr('href', downloadBaseUrl + searchParams);
+                resultsContainer.removeClass('pending-results-update');
+                filterForm.find('input').removeAttr('disabled');
+                clearLinks.removeClass('clear-disabled');
             }
         });
     }
 
+    clearLinks.on('click', function (e) {
+            e.preventDefault();
+            if (!$(this).hasClass('clear-disabled')) {
+                var $govUkSelect = $(this).parents('.govuk-option-select');
+                if ($govUkSelect.hasClass('date-filters') || $govUkSelect.hasClass('age-filter')) {
+                    $govUkSelect.find('input[type="text"]').val('');
+
+                } else {
+                    var selectedFilters = $(this)
+                        .next('.options-container')
+                        .find('input')
+                        .filter(function (n, item) {
+                            return $(item).prop('checked');
+                        });
+
+                    selectedFilters.click();
+                    $(this).removeClass('active-clear');
+                   
+                }
+            }
+        });
+
     filterForm.find('.trigger-result-update').on('change', function() {
         window.clearTimeout(filterIntent);
-        filterIntent = window.setTimeout(function() {
+        resultsContainer.addClass('pending-results-update');
+        filterIntent = window.setTimeout(function () {
+            resultsContainer.html(plsWait);
+            searchParams = $('#filter-form-ind').serialize();
+            
             getResults();
         }, 1200);
         
@@ -32,7 +71,8 @@
     filterForm.find('.filter-button').on('click', function(e) {
         e.preventDefault();
         window.clearTimeout(filterIntent);
-
+        resultsContainer.html(plsWait);
+        searchParams = $('#filter-form-ind').serialize();
         getResults();
     });
     
