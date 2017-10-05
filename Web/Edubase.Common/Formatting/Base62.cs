7783e603ded344a69b766f1550a18fd735b7e9b7
krisdyson <kris.dyson@outlook.com>
2017-10-05 21:12:29 +0100

    14963: Added the ability to save a querystring to a token and use the token in the querystring instead, avoiding the max QS length limitation.

---
+++ Web/Edubase.Common/Formatting/Base62.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Edubase.Common.Formatting
+{
+    public static class Base62
+    {
+        static string ToBase62(int value)
+        {
+            if (value < 10)
+                return value.ToString();
+            else if (value < 36)
+                return ((char)(value - 10 + 'A')).ToString();
+            else if (value < 62)
+                return ((char)(value - 36 + 'a')).ToString();
+            else
+                return ToBase62(value / 62) + ToBase62(value % 62);
+        }
+
+        public static string Encode(int value)
+        {
+            if (value <= 0 || value > 14776336)
+                throw new ArgumentOutOfRangeException();
+
+            value--;
+            return ToBase62(value / 62) + ToBase62(value % 62);
+        }
+
+        public static int Decode(string code)
+        {
+            int value = 0;
+            foreach (char c in code)
+            {
+                value *= 62;
+                if (c <= '9')
+                    value += c - '0';
+                else if (c <= 'Z')
+                    value += c - 'A' + 10;
+                else
+                    value += c - 'a' + 36;
+            }
+            return value + 1;
+        }
+    }
+}
---
+++ Web/Edubase.Data/Entity/Token.cs
@@ -0,0 +1,26 @@
+﻿using Edubase.Common;
+using Edubase.Common.Formatting;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace Edubase.Data.Entity
+{
+    public class Token : TableEntity
+    {
+        public string Data { get; set; }
+
+        [IgnoreProperty]
+        public string Id => string.Concat(PartitionKey, RowKey);
+
+        public Token()
+        {
+            PartitionKey = Base62.Encode(int.Parse(string.Concat(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day)));
+            RowKey = Base62.Encode(RandomNumber.Next(1, 10_000_000));
+        }
+
+        public Token(string data) : this()
+        {
+            Data = data;
+        }
+    }
+}
