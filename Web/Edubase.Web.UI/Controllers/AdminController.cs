c0e446ebb86f723e725adb090d8a87095a859de4
Kris Dyson <kris.dyson@outlook.com>
2018-07-03 13:23:56 +0100

    Removing log viewing as we'll use Storage Explorer to view error details

--- Web/Edubase.Web.UI/Controllers/AdminController.cs
+++ Web/Edubase.Web.UI/Controllers/AdminController.cs
@@ -24,25 +24,10 @@ namespace Edubase.Web.UI.Controllers
         private readonly ILoggingService _loggingService;
 
         public AdminController(ILoggingService loggingService) => _loggingService = loggingService;
-
-        [Route("Logs")]
-        public async Task<ActionResult> Logs(string date, string skipToken)
-        {
-            var result = await _loggingService.GetAllAsync(10, UriHelper.TryDeserializeUrlToken<TableContinuationToken>(skipToken), date.ToDateTime("yyyyMMdd"));
-            LogMessagesDto dto = new LogMessagesDto(result.Item1.Cast<WebLogMessage>(), UriHelper.SerializeToUrlToken(result.Item2));
-            return View(new LogMessagesViewModel(dto) {DateFilter = date});
-        }
-
-        [Route("LogDetail/{id}")]
-        public async Task<ActionResult> LogDetail(string id)
-        {
-            var message = (WebLogMessage) await _loggingService.GetAsync(id);
-            if (message == null) return Content("Log message not found.  Please note it may take up to 30 seconds for a log message to become available at this URL.", "text/plain");
-            return View(message);
-        }
         
         [HttpGet, Route("GetPendingErrors")]
-        public ActionResult GetPendingErrors(string pwd) {
+        public ActionResult GetPendingErrors(string pwd)
+        {
             return pwd == "c7634" ? (ActionResult) Json(_loggingService.GetPending()) : new EmptyResult();
         }
 
