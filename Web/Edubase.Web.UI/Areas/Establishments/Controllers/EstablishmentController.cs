046f62517d72475509aedee9911d9a5bbeee488e
Kris Dyson <kris.dyson@outlook.com>
2018-10-16 16:09:18 +0100

    Bug fixes

--- Web/Edubase.Web.UI/Areas/Establishments/Controllers/EstablishmentController.cs
+++ Web/Edubase.Web.UI/Areas/Establishments/Controllers/EstablishmentController.cs
@@ -694,7 +694,15 @@ namespace Edubase.Web.UI.Areas.Establishments.Controllers
         {
             if (User.Identity.IsAuthenticated)
             {
-                viewModel.ChangeHistory = await _establishmentReadService.GetChangeHistoryAsync(id, skip, take, sortBy, User);
+                try
+                {
+                    viewModel.ChangeHistory = await _establishmentReadService.GetChangeHistoryAsync(id, skip, take, sortBy, User);
+                }
+                catch
+                {
+                    // KHD: Texuna will sometimes return an API error.  I have been asked to ignore it.
+                }
+                
             }
         }
 
