957809df59b78d0a488c5cfa5689a0b1720526a8
Biggs <biggs.thorarensen@olivejar.co.uk>
2019-02-26 17:35:00 +0000

    30135 re-insert js solution tweak

--- Web/Edubase.Web.UI/Assets/Scripts/standalone/accessibility.js
+++ Web/Edubase.Web.UI/Assets/Scripts/standalone/accessibility.js
@@ -1,7 +1,7 @@
 (function (){
 
   // Re-inserting text into warning banner to trigger aria-live
-  var $bannerContent = $('.js-trigger-aria-live > .warning-message-text')
-  $bannerContent.html($($bannerContent.text());
+  var $bannerContent = $('.js-trigger-aria-live > .warning-message-text');
+  $bannerContent.html($bannerContent.text());
 
 }());
