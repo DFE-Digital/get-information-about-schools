719dfb352f314ea415fa357a8bc2c8d01b0ba302
Biggs <biggs.thorarensen@olivejar.co.uk>
2019-02-26 15:28:02 +0000

    30135 script made to trigger aria-live for simple warning messages

---
+++ Web/Edubase.Web.UI/Assets/Scripts/standalone/accessibility.js
@@ -0,0 +1,7 @@
+(function (){
+
+  // Re-inserting text into warning banner to trigger aria-live
+  var $bannerContent = $('.js-trigger-aria-live > .warning-message-text')
+  $bannerContent.html($($bannerContent.text());
+
+}());
