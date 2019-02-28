7e99f1b04a81c924b01f879733bd8020fe672e27
Biggs <biggs.thorarensen@olivejar.co.uk>
2019-02-28 11:30:15 +0000

    30156 create academy trust: re-insert script applied to trigger aria-live. button non-disabled. script waits now until document is ready.

--- Web/Edubase.Web.UI/Assets/Scripts/standalone/accessibility.js
+++ Web/Edubase.Web.UI/Assets/Scripts/standalone/accessibility.js
@@ -1,7 +1,8 @@
 (function (){
-
-  // Re-inserting text into warning banner to trigger aria-live
-  var $bannerContent = $('.js-trigger-aria-live > .warning-message-text');
-  $bannerContent.html($bannerContent.text());
-
+    $(document).ready(function(){
+        // Re-inserting text into warning banner to trigger aria-live
+        var $bannerContent = $('.js-trigger-aria-live > .warning-message-text');
+        $bannerContent.html($bannerContent.text());
+    });
 }());
+
