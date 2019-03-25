d5c8169c3333a1e5a64f7960438aee3ec0273d9a
Biggs <biggs.thorarensen@olivejar.co.uk>
2019-03-25 11:45:52 +0000

    31082 case tweak for validation ids, tabindex removed from date form legend

--- Web/Edubase.Web.UI/Validation/ValidationExtensions.cs
+++ Web/Edubase.Web.UI/Validation/ValidationExtensions.cs
@@ -3,6 +3,9 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Web.Mvc;
 using Edubase.Services.Domain;
+using Edubase.Common;
+using Edubase.Common.Text;
+using MoreLinq;
 
 namespace Edubase.Web.UI.Validation
 {
@@ -12,11 +15,20 @@ namespace Edubase.Web.UI.Validation
         {
             foreach (var error in validationEnvelope.Errors)
             {
-                if (!avoidDuplicates || 
-                    !controllerContext.Controller.ViewData.ModelState.ContainsKey(error.Fields) || 
+                if (!avoidDuplicates ||
+                    !controllerContext.Controller.ViewData.ModelState.ContainsKey(error.Fields) ||
                     !controllerContext.Controller.ViewData.ModelState[error.Fields].Errors.Any())
                 {
-                    controllerContext.Controller.ViewData.ModelState.AddModelError(error.Fields, error.GetMessage());
+                    // default the fieldname to TitleCase
+                    var fieldName = error.Fields.ToTextCase(eTextCase.TitleCase);
+                    // ensure the field name casing is not changed if it already exists within the model
+                    if (controllerContext.Controller.ViewData.ModelState.Keys.Any(x => x.StartsWith(fieldName, StringComparison.InvariantCultureIgnoreCase)))
+                    {
+                        fieldName = controllerContext.Controller.ViewData.ModelState.Keys.Contains(fieldName, StringComparer.InvariantCultureIgnoreCase) ?
+                            controllerContext.Controller.ViewData.ModelState.Keys.First(x => x.Equals(fieldName, StringComparison.InvariantCultureIgnoreCase)) :
+                            controllerContext.Controller.ViewData.ModelState.Keys.First(x => x.StartsWith(fieldName, StringComparison.InvariantCultureIgnoreCase)).Substring(0, fieldName.Length);
+                    }
+                    controllerContext.Controller.ViewData.ModelState.AddModelError(fieldName, error.GetMessage());
                 }
             }
         }
