1449db86bd016cb2987dee9517d7d773e4421129
Michael Evely <Michael.Evely@olivejar.co.uk>
2019-03-29 15:11:14 +0000

    31092 - form errors - edit governance delegation message length validation message no longer includes duplicates

--- Web/Edubase.Web.UI/Helpers/ValidationExtensions.cs
+++ Web/Edubase.Web.UI/Helpers/ValidationExtensions.cs
@@ -1,13 +1,14 @@
-﻿using FluentValidation.Results;
+using FluentValidation.Results;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace Edubase.Web.UI.Helpers
 {
     public static class ValidationExtensions
     {
-        public static void EduBaseAddToModelState(this ValidationResult result, ModelStateDictionary modelState, string prefix)
+        public static void EduBaseAddToModelState(this ValidationResult result, ModelStateDictionary modelState, string prefix, bool avoidDuplicates = false)
         {
             if (result.IsValid)
                 return;
@@ -15,8 +16,14 @@ namespace Edubase.Web.UI.Helpers
             foreach (var error in result.Errors)
             {
                 var key = string.IsNullOrEmpty(prefix) ? error.PropertyName : prefix + "." + error.PropertyName;
-                modelState.AddModelError(key, error.CustomState?.ToString() ?? error.ErrorMessage);
-                modelState.SetModelValue(key, new ValueProviderResult(error.AttemptedValue ?? "", (error.AttemptedValue ?? "").ToString(), CultureInfo.CurrentCulture));
+
+                if (!avoidDuplicates ||
+                    !modelState.ContainsKey(key) ||
+                    !modelState[key].Errors.Any())
+                {
+                    modelState.AddModelError(key, error.CustomState?.ToString() ?? error.ErrorMessage);
+                    modelState.SetModelValue(key, new ValueProviderResult(error.AttemptedValue ?? "", (error.AttemptedValue ?? "").ToString(), CultureInfo.CurrentCulture));
+                }
             }
         }
     }
