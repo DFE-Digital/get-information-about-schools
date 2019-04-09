15cb92e82b4790bf1c423217f40b3de7a8607123
Michael Evely <Michael.Evely@olivejar.co.uk>
2019-04-09 16:10:12 +0100

    31473 - original establishment name and type maintained in the page title/display during edit attempt where either a confirmation or validation is required.

--- Web/Edubase.Web.UI/Areas/Establishments/Controllers/EstablishmentController.cs
+++ Web/Edubase.Web.UI/Areas/Establishments/Controllers/EstablishmentController.cs
@@ -1020,6 +1020,8 @@ namespace Edubase.Web.UI.Areas.Establishments.Controllers
             if (viewModel.ActionSpecifierCommand == ViewModel.ASSave || viewModel.ActionSpecifierCommand == ViewModel.ASConfirm)
             {
                 var originalEstabTypeId = (ET) domainModel.TypeId;
+                var originalName = domainModel.Name;
+
                 await ValidateAsync(viewModel, domainModel, (ModelState.IsValid || viewModel.ActionSpecifierCommand == ViewModel.ASConfirm));
                 var newEstabTypeId = (ET?) domainModel.TypeId;
 
@@ -1042,6 +1044,11 @@ namespace Edubase.Web.UI.Areas.Establishments.Controllers
                     }
                     else return Redirect(Url.RouteUrl("EstabDetails", new { id = viewModel.Urn.Value }) + viewModel.SelectedTab2DetailPageTabNameMapping[viewModel.SelectedTab]);
                 }
+                else
+                {
+                    viewModel.OriginalEstablishmentName = originalName;
+                    viewModel.OriginalTypeName = (await _cachedLookupService.EstablishmentTypesGetAllAsync()).Where(x => x.Id == (int)originalEstabTypeId).Select(x => x.Name).FirstOrDefault();
+                }
             }
             else if (viewModel.ActionSpecifierCommand == ViewModel.ASAddAddress)
             {
@@ -1050,6 +1057,11 @@ namespace Edubase.Web.UI.Areas.Establishments.Controllers
             else if (viewModel.ActionSpecifierCommand == ViewModel.ASRemoveAddress)
             {
                 RemoveAdditionalAddress(viewModel);
+            }
+            else if (viewModel.ActionSpecifierCommand == ViewModel.ASCancel)
+            {   
+                viewModel.OriginalEstablishmentName = domainModel.Name;
+                viewModel.OriginalTypeName = (await _cachedLookupService.EstablishmentTypesGetAllAsync()).Where(x => x.Id == domainModel.TypeId).Select(x => x.Name).FirstOrDefault();
             }
 
             return View(viewModel);
