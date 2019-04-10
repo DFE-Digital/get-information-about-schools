2975a3b0f81895ee57eabd9cf4cdb0074bf5e29b
Michael Evely <Michael.Evely@olivejar.co.uk>
2019-04-10 12:59:17 +0100

    Revert "31657 - Governance Bulk Update errors returning to incorrect URL which includes /Index in the address"
    This reverts commit a4569d78562517de7eb930b8d4e22142be00e183.

--- Web/Edubase.Web.UI/Areas/Governors/Controllers/GovernorsBulkUpdateController.cs
+++ Web/Edubase.Web.UI/Areas/Governors/Controllers/GovernorsBulkUpdateController.cs
@@ -1,4 +1,4 @@
-using Edubase.Common.IO;
+﻿using Edubase.Common.IO;
 using Edubase.Services.Governors;
 using Edubase.Web.UI.Areas.Governors.Models;
 using Edubase.Web.UI.Helpers;
@@ -11,7 +11,7 @@ namespace Edubase.Web.UI.Areas.Governors.Controllers
 {
     using R = Services.Security.EdubaseRoles;
 
-    [RouteArea("Governors"), RoutePrefix("BulkUpdate"), MvcAuthorizeRoles(R.EDUBASE_GROUP_MAT, R.ESTABLISHMENT, R.EFADO, R.ROLE_BACKOFFICE)]
+    [RouteArea("Governors"), RoutePrefix("BulkUpdate"), Route("{action=index}"), MvcAuthorizeRoles(R.EDUBASE_GROUP_MAT, R.ESTABLISHMENT, R.EFADO, R.ROLE_BACKOFFICE)]
     public class GovernorsBulkUpdateController : Controller
     {
         readonly IGovernorsWriteService _governorsWriteService;
@@ -34,7 +34,7 @@ namespace Edubase.Web.UI.Areas.Governors.Controllers
         }
 
 
-        [HttpPost, Route(Name = "GovernorsProcessBulkUpdate")]
+        [HttpPost, Route("Index", Name = "GovernorsProcessBulkUpdate")]
         public async Task<ActionResult> ProcessBulkUpdate(GovernorsBulkUpdateViewModel viewModel)
         {
             if (ModelState.IsValid)
