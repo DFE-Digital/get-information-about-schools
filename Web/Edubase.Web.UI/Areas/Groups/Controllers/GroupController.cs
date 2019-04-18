b766e97824dd2d937472b60354b25f5d55fb8f3e
Michael Evely <Michael.Evely@olivejar.co.uk>
2019-04-18 14:24:44 +0100

    32040 - Convert academy trust - add error message for attempting to convert a closed single academy trust

--- Web/Edubase.Web.UI/Areas/Groups/Controllers/GroupController.cs
+++ Web/Edubase.Web.UI/Areas/Groups/Controllers/GroupController.cs
@@ -70,6 +70,7 @@ namespace Edubase.Web.UI.Areas.Groups.Controllers
             {
                 var result = (await _groupReadService.SearchByIdsAsync(viewModel.Text, viewModel.Text.ToInteger(), viewModel.Text, User)).Items.FirstOrDefault();
                 if (result == null) ModelState.AddModelError(nameof(viewModel.Text), "We were unable to find a single academy trust matching those details");
+                else if (result.StatusId == ((int) GS.Closed)) ModelState.AddModelError(nameof(viewModel.Text), "Closed single academy trusts can not be converted");
                 else if (result.GroupTypeId != (int) GT.SingleacademyTrust) ModelState.AddModelError(nameof(viewModel.Text), "That's an invalid group because it's of the wrong type.");
                 else
                 {
