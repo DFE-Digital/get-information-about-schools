2fdda71bbf2a9428a0839625cd815b0d7a7c2d8d
Michael Evely <Michael.Evely@olivejar.co.uk>
2019-10-14 15:47:09 +0100

    37243 - EstablishmentApi controller (middle-tier) now only returns the bare minimul as part of the response to the client. This is actually just the name and urn

--- Web/Edubase.Web.UI/Controllers/Api/EstablishmentApiController.cs
+++ Web/Edubase.Web.UI/Controllers/Api/EstablishmentApiController.cs
@@ -1,9 +1,11 @@
+using System.Dynamic;
 using Edubase.Services.Domain;
 using Edubase.Services.Establishments;
 using Edubase.Services.Establishments.Models;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Edubase.Web.UI.Filters;
+using Newtonsoft.Json.Linq;
 
 namespace Edubase.Web.UI.Controllers.Api
 {
@@ -22,7 +24,31 @@ namespace Edubase.Web.UI.Controllers.Api
         {
             var retVal = await _establishmentReadService.GetAsync(urn, User);
             if (retVal.ReturnValue == null) return NotFound();
-            else return Ok(retVal);
+            else
+            {
+                var apiResult = retVal.GetResult();
+                var minRes = new EstbalishmentApiResponse
+                {
+                    Status = "Success",
+                    ReturnValue = new EstablishmentApiPayload()
+                    {
+                        Name = apiResult.Name,
+                        Urn = apiResult.Urn
+                    }
+                };
+                return Ok(minRes);
+            }
+        }
+
+        public class EstablishmentApiPayload
+        {
+            public string Name { get; set; }
+            public int? Urn { get; set; }
+        }
+        public class EstbalishmentApiResponse
+        {
+            public string Status { get; set; }
+            public EstablishmentApiPayload ReturnValue { get; set; }
         }
     }
 }
