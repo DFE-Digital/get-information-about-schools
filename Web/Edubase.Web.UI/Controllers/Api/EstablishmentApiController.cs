9bd3ed15980479fac7777528f8a11522427a4f8f
Michael Evely <Michael.Evely@olivejar.co.uk>
2019-10-14 13:28:50 +0100

    37243 - authentication wrapped around the establishment api controller so only DSI logged in accounts can retrieve the api response

--- Web/Edubase.Web.UI/Controllers/Api/EstablishmentApiController.cs
+++ Web/Edubase.Web.UI/Controllers/Api/EstablishmentApiController.cs
@@ -1,11 +1,13 @@
-﻿using Edubase.Services.Domain;
+using Edubase.Services.Domain;
 using Edubase.Services.Establishments;
 using Edubase.Services.Establishments.Models;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Edubase.Web.UI.Filters;
 
 namespace Edubase.Web.UI.Controllers.Api
 {
+    [Authorize]
     public class EstablishmentApiController : ApiController
     {
         private readonly IEstablishmentReadService _establishmentReadService;
@@ -15,7 +17,7 @@ namespace Edubase.Web.UI.Controllers.Api
             _establishmentReadService = establishmentReadService;
         }
 
-        [Route("api/establishment/{urn:int}"), HttpGet]
+        [Route("api/establishment/{urn:int}"), HttpGet, EdubaseAuthorize]
         public async Task<IHttpActionResult> Get(int urn)
         {
             var retVal = await _establishmentReadService.GetAsync(urn, User);
