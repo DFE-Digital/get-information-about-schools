e6edabe364efb850f44991cec4d4fd0be4329a5c
Michael Evely <Michael.Evely@olivejar.co.uk>
2019-10-21 11:54:42 +0100

    Removal of api/establishment-search as no longer required

--- Web/Edubase.Web.UI/Controllers/Api/SearchApiController.cs
+++ Web/Edubase.Web.UI/Controllers/Api/SearchApiController.cs
@@ -1,10 +1,6 @@
-﻿using Edubase.Data.Entity;
+using Edubase.Data.Entity;
 using Edubase.Data.Repositories;
-using Edubase.Services.Domain;
 using Edubase.Services.Establishments;
-using Edubase.Services.Establishments.Models;
-using Edubase.Services.Establishments.Search;
-using System;
 using System.Threading.Tasks;
 using System.Web.Http;
 
@@ -12,18 +8,12 @@ namespace Edubase.Web.UI.Controllers.Api
 {
     public class SearchApiController : ApiController
     {
-        private readonly IEstablishmentReadService _establishmentReadService;
         private readonly ITokenRepository _tokenRepository;
 
-        public SearchApiController(IEstablishmentReadService establishmentReadService, ITokenRepository tokenRepository)
+        public SearchApiController(ITokenRepository tokenRepository)
         {
-            _establishmentReadService = establishmentReadService;
             _tokenRepository = tokenRepository;
         }
-
-        [Route("api/establishment-search"), HttpPost]
-        public async Task<ApiPagedResult<EstablishmentSearchResultModel>> ProcessRequestAsync(EstablishmentSearchPayload payload) 
-            => await _establishmentReadService.SearchAsync(payload, User);
 
         [Route("api/tokenize"), HttpPost]
         public async Task<dynamic> Tokenize()
