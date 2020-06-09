22999bb2449de9fbaa5d073bffff0ae6a14be6e3
Michael Evely <Michael.Evely@olivejar.co.uk>
2020-06-09 14:53:50 +0100

    41831 - bulk update page transformed to PRG pattern

---
+++ Web/Edubase.Web.UI/Filters/ModelStateTransfer.cs
@@ -0,0 +1,48 @@
+using System.Web.Mvc;
+namespace Edubase.Web.UI.Filters
+{
+    public abstract class ModelStateTransfer : ActionFilterAttribute
+    {
+        protected static readonly string Key = typeof(ModelStateTransfer).FullName;
+    }
+
+    public class ExportModelStateAttribute : ModelStateTransfer
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            //Only export when ModelState is not valid
+            if (!filterContext.Controller.ViewData.ModelState.IsValid)
+            {
+                //Export if we are redirecting
+                if ((filterContext.Result is RedirectResult) || (filterContext.Result is RedirectToRouteResult))
+                {
+                    filterContext.Controller.TempData[Key] = filterContext.Controller.ViewData.ModelState;
+                }
+            }
+
+            base.OnActionExecuted(filterContext);
+        }
+    }
+
+    public class ImportModelStateAttribute : ModelStateTransfer
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (filterContext.Controller.TempData[Key] is ModelStateDictionary modelState)
+            {
+                //Only Import if we are viewing
+                if (filterContext.Result is ViewResult)
+                {
+                    filterContext.Controller.ViewData.ModelState.Merge(modelState);
+                }
+                else
+                {
+                    //Otherwise remove it.
+                    filterContext.Controller.TempData.Remove(Key);
+                }
+            }
+
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
