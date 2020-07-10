e14de8e46022a31e70d0db9970f3d1e11d7e37b3
Jon Banner <jonbanner@outlook.com>
2020-07-10 16:21:38 +0100

    39828 GDSmig: Bulk academies tool

---
+++ Web/Edubase.Web.UI/Assets/Scripts/Entry/bulk-academies.js
@@ -0,0 +1,19 @@
+import GiasAttachUnload from '../GiasModules/GiasModals/GiasAttachUnload';
+
+const $mc = $('#main-content');
+const $fields = $mc.find(':input');
+
+
+new GiasAttachUnload({
+  fields: $fields
+});
+
+$mc.find('.remove-urn').okCancel({
+  triggerEvent: 'click',
+  title: 'Are you sure you want to remove this establishment?',
+  content: 'This will permanently remove it from the list.',
+  ok: function () {
+    $(this.el).data().okCancel.unbind();
+    $(this.el).click();
+  }
+});
