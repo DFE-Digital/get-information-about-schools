c57a9565fbc6702b28895fb7b7feac4f04007cd0
Jon Banner <jonbanner@outlook.com>
2020-09-23 10:40:36 +0100

    43475 modal confirmation editing group estabs

--- Web/Edubase.Web.UI/Assets/Scripts/Entry/edit-group-links.js
+++ Web/Edubase.Web.UI/Assets/Scripts/Entry/edit-group-links.js
@@ -1,9 +1,13 @@
 import GiasAttachUnload from '../GiasModules/GiasModals/GiasAttachUnload';
+const isFederation = document.getElementById('GroupTypeName') && document.getElementById('GroupTypeName').value === 'Federation';
+
+const modalTitle = isFederation ? 'Are you sure you want to remove this school?': 'Are you sure you want to remove this academy?';
+const modalContent = isFederation ? 'All school information will be removed.' : 'All governor information associated with this academy will be removed too.';
 
 $('#main-content').find('.button-remove').okCancel({
   triggerEvent: 'click',
-  title: 'Are you sure you want to remove this academy?',
-  content: 'All governor information associated with this academy will be removed too.',
+  title: modalTitle,
+  content: modalContent,
   idPrefix: 'remove-',
   ok: function () {
     $(this.el).data().okCancel.unbind();
