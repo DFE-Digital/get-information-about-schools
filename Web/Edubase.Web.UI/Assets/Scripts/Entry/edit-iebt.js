2fd08f6643ddf96f82d6613eda3b7ed454f9693b
Jon Banner <jonbanner@outlook.com>
2020-10-07 17:21:45 +0100

    43848 js error iebt

--- Web/Edubase.Web.UI/Assets/Scripts/Entry/edit-iebt.js
+++ Web/Edubase.Web.UI/Assets/Scripts/Entry/edit-iebt.js
@@ -4,5 +4,11 @@ import GiasTextCounter from '../GiasModules/GiasTextCounter';
 iebtRadios.init();
 
 new GiasAttachUnload();
-new GiasTextCounter(document.getElementById('Notes'), { maxLength: 4000 });
-new GiasTextCounter(document.getElementById('Associations'), { maxLength: 1000 });
+if (document.getElementById('Notes')){
+  new GiasTextCounter(document.getElementById('Notes'), { maxLength: 4000 });
+}
+
+if (document.getElementById('Associations')){
+  new GiasTextCounter(document.getElementById('Associations'), { maxLength: 1000 });
+}
+
