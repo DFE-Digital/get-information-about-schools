2299572893781f826b8d34df14bbd24021d4acc9
Jon Banner <jonbanner@outlook.com>
2020-11-18 16:55:40 +0000

    trying to get around safari bug where focus styles aren't applied 3

--- Web/Edubase.Web.UI/Assets/Scripts/GiasSearchFilters/GiasNestedFilters.js
+++ Web/Edubase.Web.UI/Assets/Scripts/GiasSearchFilters/GiasNestedFilters.js
@@ -75,13 +75,6 @@ class GiasNestedFilters {
       $(this).attr('aria-expanded', $(this).hasClass('open-children'));
     });
 
-    trigger.on('focus', function(){
-      $(this).css({ outline: '1px dotted #0b0c0c'});
-    });
-
-    trigger.on('blur', function(){
-      $(this).css({ outline: 'none'});
-    });
 
     parent.on('change', function (e) {
       const optSelect = $(this).parents('.govuk-option-select');
