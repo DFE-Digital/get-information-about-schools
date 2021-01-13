fe2f58c8079ccf5d93940784717117d303ccc9d5
Jon Banner <jonbanner@outlook.com>
2021-01-13 15:41:44 +0000

    40726 merge-amalgamate no-js

---
+++ Web/Edubase.Web.UI/Models/Tools/AmalgamateEstablishmentsModel.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using Edubase.Services.Domain;
+
+namespace Edubase.Web.UI.Models.Tools
+{
+    public class AmalgamateEstablishmentsModel
+    {
+        public int? Establishment0Urn { get; set; }
+
+        public int? Establishment1Urn { get; set; }
+
+        public int? Establishment2Urn { get; set; }
+
+        public int? Establishment3Urn { get; set; }
+
+        public string Establishment0Name { get; set; }
+
+        public string Establishment1Name { get; set; }
+
+        public string Establishment2Name { get; set; }
+
+        public string Establishment3Name { get; set; }
+
+        [Display(Name = "Merge date")]
+        public DateTimeViewModel MergeDate { get; set; } = new DateTimeViewModel();
+
+        public int? NewEstablishmentUrn { get; set; }
+        public string NewEstablishmentName { get; set; }
+        public string EstablishmentType { get; set; }
+
+        public int? EstablishmentPhase { get; set; }
+
+        public string LocalAuthorityId { get; set; }
+
+        public string LocalAuthorityName { get; set; }
+
+        public IEnumerable<SelectListItem> EstablishmentTypes { get; set; }
+
+        public IEnumerable<SelectListItem> EstablishmentPhases { get; set; }
+
+        public IEnumerable<SelectListItem> LocalAuthorities { get; set; }
+    }
+}
