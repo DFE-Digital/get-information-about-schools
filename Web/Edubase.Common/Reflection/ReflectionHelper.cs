d11c166aea1a71a999a9e63e18befb8ec651602d
MEvely <MEvely@MichaelEvely.com>
2021-04-21 14:20:23 +0100

    45696 - group details now have confirmation page shown for sats and mats

--- Web/Edubase.Common/Reflection/ReflectionHelper.cs
+++ Web/Edubase.Common/Reflection/ReflectionHelper.cs
@@ -65,10 +65,11 @@ namespace Edubase.Common.Reflection
                 .Select(p => p.Name).ToList();
         }
 
-
-
-
-
+        
+        public static List<ChangeDescriptor> DetectGroupChanges(object newModel, object oldModel, params Type[] types)
+        {
+            return DetectChanges(newModel, oldModel, "Group", types);
+        }
 
         public static List<ChangeDescriptor> DetectChanges(object newModel, object oldModel, params Type[] types)
         {
