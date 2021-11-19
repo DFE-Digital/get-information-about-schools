83919eab4c3a35ac91bf858668bc307895b7c307
Adam MILLARD <Adam.MILLARD@education.gov.uk>
2021-11-19 11:47:42 +0000

    48703 - fixed openonly chechbox preference for LA disambiguation

--- Web/Edubase.Web.UI/Models/Search/LocalAuthorityDisambiguationViewModel.cs
+++ Web/Edubase.Web.UI/Models/Search/LocalAuthorityDisambiguationViewModel.cs
@@ -1,4 +1,4 @@
-﻿using Edubase.Services.Domain;
+using Edubase.Services.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,17 +11,19 @@ namespace Edubase.Web.UI.Models.Search
         public List<int> SelectedLocalAuthorityIds { get; set; } = new List<int>();
         public List<LookupDto> MatchingLocalAuthorities { get; set; } = new List<LookupDto>();
         public string SearchText { get; set; }
+        public bool OpenOnly { get; set; }
 
         public LocalAuthorityDisambiguationViewModel()
         {
 
         }
 
-        public LocalAuthorityDisambiguationViewModel(List<int> selectedLocalAuthorityIds, string searchText, List<LookupDto> matchingLocalAuthorities)
+        public LocalAuthorityDisambiguationViewModel(List<int> selectedLocalAuthorityIds, string searchText, List<LookupDto> matchingLocalAuthorities, bool openOnly)
         {
             SelectedLocalAuthorityIds = selectedLocalAuthorityIds;
             SearchText = searchText;
             MatchingLocalAuthorities = matchingLocalAuthorities;
+            OpenOnly = openOnly;
         }
     }
 }
