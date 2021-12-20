f77e646b78ae1950e52fd4621fc23a26812d0523
Scott Dawson <Scott.DAWSON@education.gov.uk>
2021-12-20 16:28:35 +0000

    Got first Xunit tests running

---
+++ Web/Edubase.CommonTests/DisposerTests.cs
@@ -0,0 +1,31 @@
+using Xunit;
+using Edubase.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edubase.Common.Tests
+{
+    public class DisposerTests
+    {
+        [Fact()]
+        public void TimedTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void UsingTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void DisposeTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
