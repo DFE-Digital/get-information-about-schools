8afa7c31fc769ddd0739300ce30af223e2a03dc3
Christopher Morriss <Christopher.MORRISS@EDUCATION.GOV.UK>
2022-08-25 13:30:23 +0100

    added LaNameCode model view and controller

--- Web/Edubase.Web.UI/Controllers/GuidanceController.cs
+++ Web/Edubase.Web.UI/Controllers/GuidanceController.cs
@@ -1,13 +1,29 @@
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using CsvHelper;
+using Edubase.Services;
+using Edubase.Web.UI.Models.Guidance;
+using Microsoft.Data.Edm.Csdl;
 
 namespace Edubase.Web.UI.Controllers
 {
     [RoutePrefix("Guidance"), Route("{action=index}")]
     public class GuidanceController : EduBaseController
     {
+        private readonly IBlobService _blobService;
+
+        public GuidanceController(IBlobService blobService)
+        {
+            _blobService = blobService;
+        }
+
         [Route(Name = "Guidance")]
         public ActionResult Index() => View();
-       
+
         public ActionResult General() => View();
 
         public ActionResult EstablishmentBulkUpdate() => View();
@@ -15,5 +31,52 @@ namespace Edubase.Web.UI.Controllers
         public ActionResult ChildrensCentre() => View();
         public ActionResult Federation() => View();
         public ActionResult Governance() => View();
+        // public ActionResult LaNameCodes() => View();
+
+        public async Task<ActionResult> LaNameCodes()
+        {
+            var viewModel = new GuidanceLaNameCodeViewModel();
+
+            //populate viewmodel with data from blob
+            var data = await GetCsvFromContainer("guidance", "EnglishLaNameCodes.csv");
+            using (var streamReader = new StreamReader(data))
+            using (var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture))
+            {
+                var records = csvReader.GetRecords<LaNameCodes>().ToList();
+            }
+
+            return View();
+        }
+
+        private async Task<string> GetCsvFromContainer(string container, string file)
+        {
+            var blob = _blobService.GetBlobReference(container, file);
+            if (await blob.ExistsAsync())
+            {
+                return blob.DownloadTextAsync().Result;
+                // return contents;
+                //var stream = await blob.OpenReadAsync();
+                //return new FileStreamResult(stream, blob.Properties.ContentType)
+                //{
+                //    FileDownloadName = blob.Name
+                //};
+            }
+            throw new Exception("File not available");
+        }
+
+        private async Task<ActionResult> GetFileFromContainer(string container, string file)
+        {
+            var blob = _blobService.GetBlobReference(container, file);
+            if (await blob.ExistsAsync())
+            {
+                var stream = await blob.OpenReadAsync();
+                return new FileStreamResult(stream, blob.Properties.ContentType)
+                {
+                    FileDownloadName = blob.Name
+                };
+            }
+            throw new Exception("File not available");
+        }
     }
 }
+
---
+++ Web/Edubase.Web.UI/Models/Guidance/GuidanceLaNameCodeViewModel.cs
@@ -0,0 +1,10 @@
+namespace Edubase.Web.UI.Models.Guidance
+{
+    public class GuidanceLaNameCodeViewModel
+    {
+        public string DownloadType { get; set; }
+        public LaNameCodes EnglishLas { get; set; }
+        public LaNameCodes WelshLas { get; set; }
+        public LaNameCodes OtherLas { get; set; }
+    }
+}
