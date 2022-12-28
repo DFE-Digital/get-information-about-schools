fa5f798dc62603aae6c1c56a262d56c5a878d068
Scott Dawson <scott.dawson@education.gov.uk>
2022-12-28 15:15:50 +0000

    Removed obsolete unused code - and requirement for it from interface

--- Web/Edubase.Services.Texuna/ChangeHistory/ChangeHistoryService.cs
+++ Web/Edubase.Services.Texuna/ChangeHistory/ChangeHistoryService.cs
@@ -1,9 +1,9 @@
-using Edubase.Services.Domain;
-using Edubase.Services.Texuna.ChangeHistory.Models;
 using System;
 using System.Collections.Generic;
 using System.Security.Principal;
 using System.Threading.Tasks;
+using Edubase.Services.Domain;
+using Edubase.Services.Texuna.ChangeHistory.Models;
 
 namespace Edubase.Services.Texuna.ChangeHistory
 {
@@ -17,24 +17,34 @@ namespace Edubase.Services.Texuna.ChangeHistory
         }
 
         public async Task<List<EstablishmentField>> GetEstablishmentFieldsAsync(IPrincipal principal)
-            => (await _httpClient.GetAsync<List<EstablishmentField>>("establishment/change-history/fields", principal)).GetResponse();
-
-        public async Task<ApiPagedResult<ChangeHistorySearchItem>> SearchAsync(SearchChangeHistoryBrowsePayload payload, IPrincipal principal)
-            => (await _httpClient.PostAsync<ApiPagedResult<ChangeHistorySearchItem>>("change-history", payload, principal)).GetResponse();
-
-        public async Task<List<UserGroupModel>> GetSuggesterGroupsAsync(IPrincipal principal)
-            => (await _httpClient.GetAsync<List<UserGroupModel>>("groups/suggesters", principal)).GetResponse();
-
-        [Obsolete("Please use GetSuggesterGroupsAsync", true)]
-        public async Task<List<UserGroupModel>> GetApproversGroupsAsync(IPrincipal principal)
         {
-            return await GetSuggesterGroupsAsync(principal);
+            return (await _httpClient.GetAsync<List<EstablishmentField>>("establishment/change-history/fields",
+                principal)).GetResponse();
         }
 
-        public async Task<ProgressDto> SearchWithDownloadGenerationAsync(SearchChangeHistoryDownloadPayload payload, IPrincipal principal)
-            => (await _httpClient.PostAsync<ProgressDto>("change-history/download", payload, principal)).GetResponse();
+        public async Task<ApiPagedResult<ChangeHistorySearchItem>> SearchAsync(SearchChangeHistoryBrowsePayload payload,
+            IPrincipal principal)
+        {
+            return (await _httpClient.PostAsync<ApiPagedResult<ChangeHistorySearchItem>>("change-history", payload,
+                principal)).GetResponse();
+        }
+
+        public async Task<List<UserGroupModel>> GetSuggesterGroupsAsync(IPrincipal principal)
+        {
+            return (await _httpClient.GetAsync<List<UserGroupModel>>("groups/suggesters", principal)).GetResponse();
+        }
+
+        public async Task<ProgressDto> SearchWithDownloadGenerationAsync(SearchChangeHistoryDownloadPayload payload,
+            IPrincipal principal)
+        {
+            return (await _httpClient.PostAsync<ProgressDto>("change-history/download", payload, principal))
+                .GetResponse();
+        }
 
         public async Task<ProgressDto> GetDownloadGenerationProgressAsync(Guid taskId, IPrincipal principal)
-            => (await _httpClient.GetAsync<ProgressDto>($"change-history/download/progress/{taskId}", principal)).GetResponse();
+        {
+            return (await _httpClient.GetAsync<ProgressDto>($"change-history/download/progress/{taskId}", principal))
+                .GetResponse();
+        }
     }
 }
--- Web/Edubase.Services.Texuna/ChangeHistory/IChangeHistoryService.cs
+++ Web/Edubase.Services.Texuna/ChangeHistory/IChangeHistoryService.cs
@@ -1,9 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Security.Principal;
 using System.Threading.Tasks;
 using Edubase.Services.Domain;
 using Edubase.Services.Texuna.ChangeHistory.Models;
-using System.Collections.Generic;
 
 namespace Edubase.Services.Texuna.ChangeHistory
 {
@@ -11,9 +11,13 @@ namespace Edubase.Services.Texuna.ChangeHistory
     {
         Task<ProgressDto> GetDownloadGenerationProgressAsync(Guid taskId, IPrincipal principal);
         Task<List<EstablishmentField>> GetEstablishmentFieldsAsync(IPrincipal principal);
-        Task<ApiPagedResult<ChangeHistorySearchItem>> SearchAsync(SearchChangeHistoryBrowsePayload payload, IPrincipal principal);
-        Task<ProgressDto> SearchWithDownloadGenerationAsync(SearchChangeHistoryDownloadPayload payload, IPrincipal principal);
+
+        Task<ApiPagedResult<ChangeHistorySearchItem>> SearchAsync(SearchChangeHistoryBrowsePayload payload,
+            IPrincipal principal);
+
+        Task<ProgressDto> SearchWithDownloadGenerationAsync(SearchChangeHistoryDownloadPayload payload,
+            IPrincipal principal);
+
         Task<List<UserGroupModel>> GetSuggesterGroupsAsync(IPrincipal principal);
-        Task<List<UserGroupModel>> GetApproversGroupsAsync(IPrincipal principal);
     }
 }
