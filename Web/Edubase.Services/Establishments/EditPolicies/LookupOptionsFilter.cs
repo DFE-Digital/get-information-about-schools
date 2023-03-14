7cf9462b53b085f18f7be20d2123cb313ffed1a8
CLAWLOR <catherine.lawlor@education.gov.uk>
2023-03-14 11:55:39 +0000

    filter establishment statuses to only those applicable when editing establishments (16-19)

---
+++ Web/Edubase.Services/Establishments/EditPolicies/LookupOptionsFilter.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
+using Edubase.Services.Domain;
+using Edubase.Services.Enums;
+using ET = Edubase.Services.Enums.eLookupEstablishmentType;
+
+namespace Edubase.Services.Establishments.EditPolicies
+{
+    public static class LookupOptionsFilter
+    {
+        private static readonly List<int> secureAcademies16to19StatusIds = new List<int> {
+            (int) eLookupEstablishmentStatus.PendingApproval,
+            (int) eLookupEstablishmentStatus.Open,
+            (int) eLookupEstablishmentStatus.ProposedToOpen,
+            (int) eLookupEstablishmentStatus.RejectedOpening,
+            (int) eLookupEstablishmentStatus.OpenButProposedToClose,
+            (int) eLookupEstablishmentStatus.CreatedInError,
+            (int) eLookupEstablishmentStatus.Closed
+        };
+
+        public static IEnumerable<LookupDto> FilterForEstablishmentType(this IEnumerable<LookupDto> lookups, int? typeId)
+        {
+            return typeId == (int) ET.SecureAcademies16to19 ? lookups.Where(x => secureAcademies16to19StatusIds.Contains(x.Id)) : lookups;
+        }
+    }
+}
