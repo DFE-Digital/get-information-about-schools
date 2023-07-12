a4a19151b0cc6a23b7dd8863bd20a252288d80d8
CLAWLOR <catherine.lawlor@education.gov.uk>
2023-07-12 16:52:58 +0100

    Logic to show edit button on tabs modief to use EditPolicy settings instead of DisplayPolicy settings. TabDisplayPolicy modified to set location to true if any of the Locations fields are editable

--- Web/Edubase.Web.UI/Areas/Establishments/Models/TabEditPolicy.cs
+++ Web/Edubase.Web.UI/Areas/Establishments/Models/TabEditPolicy.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Security.Principal;
 using Edubase.Common;
 using Edubase.Services.Enums;
@@ -22,6 +23,11 @@ namespace Edubase.Web.UI.Areas.Establishments.Models
         public TabEditPolicy(EstablishmentModel model, EstablishmentDisplayEditPolicy policy, IPrincipal principal)
         {
             Governance = !(principal.IsInRole(EdubaseRoles.ESTABLISHMENT) && model.StatusId.OneOfThese(ES.Closed));
+            Location = new[]
+            {
+                policy.RSCRegionId, policy.GovernmentOfficeRegionId, policy.AdministrativeDistrictId, policy.AdministrativeWardId, policy.ParliamentaryConstituencyId,
+                policy.UrbanRuralId, policy.GSSLAId, policy.Easting, policy.Northing, policy.MSOAId, policy.LSOAId
+            }.Any(x => x == true);
         }
 
         public TabEditPolicy()
