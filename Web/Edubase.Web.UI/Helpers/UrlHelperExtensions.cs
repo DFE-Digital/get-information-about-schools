8b2f8dfeeaf4cd427b205f54ce7924a74c91d6b4
Roger Howell <roger.howell@education.gov.uk>
2023-08-22 10:19:15 +0100

    Update `AbsoluteActionUrl` (used only by the Site Map builder) to account for `x-forwarded-host` header.
    #157607 #172314

--- Web/Edubase.Web.UI/Helpers/UrlHelperExtensions.cs
+++ Web/Edubase.Web.UI/Helpers/UrlHelperExtensions.cs
@@ -126,14 +126,32 @@ namespace Edubase.Web.UI.Helpers
             return sb.ToString();
         }
 
+        // Note that this is used only by the SiteMap builder code
         public static string AbsoluteActionUrl(
             this UrlHelper urlHelper,
             string actionName,
             string controllerName,
             object routeValues = null)
         {
-            var scheme = urlHelper.RequestContext.HttpContext.Request.Url.Scheme;
-            return urlHelper.Action(actionName, controllerName, routeValues, scheme);
+            var forwardedHeaderAwareUrl = GetForwardedHeaderAwareUrl(urlHelper);
+
+            // Note: Providing the scheme pushes `.Action` to return an absolute URL. Omitting this appears to return a relative URL.
+            var scheme = forwardedHeaderAwareUrl.Scheme;
+            var absoluteActionUrl = urlHelper.Action(actionName, controllerName, routeValues, scheme);
+            if (absoluteActionUrl is null)
+            {
+                return null;
+            }
+
+            // Where the site is accessed via a reverse proxy, the `Host` property of the `UriBuilder` will be incorrect.
+            // For this reason, we replace the `Host` property with the value of the `X-Forwarded-Host` header where present.
+            var uriBuilder = new UriBuilder(absoluteActionUrl)
+            {
+                Host = forwardedHeaderAwareUrl.Host,
+            };
+
+            var newUri = uriBuilder.Uri;
+            return newUri.ToString();
         }
     }
 }
