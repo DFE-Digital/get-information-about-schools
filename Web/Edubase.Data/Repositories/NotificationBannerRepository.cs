eea8c892e2b31a0bc5990e4477da86f1e1c77ae9
Scott Dawson <scott.dawson@education.gov.uk>
2023-12-20 16:41:35 +0000

    Made methods for notification banner from template sync all the way and added MemoryCache

--- Web/Edubase.Data/Repositories/NotificationBannerRepository.cs
+++ Web/Edubase.Data/Repositories/NotificationBannerRepository.cs
@@ -1,30 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Threading;
+using System.Threading.Tasks;
 using Edubase.Data.Entity;
 using Edubase.Data.Repositories.TableStorage;
 using Microsoft.WindowsAzure.Storage.Table;
-using System.Threading.Tasks;
-using MoreLinq;
-using System.Collections.Generic;
-using System.Linq;
-using System;
 using Microsoft.WindowsAzure.Storage.Table.Queryable;
+using MoreLinq;
 
 namespace Edubase.Data.Repositories
 {
-    public class NotificationBannerRepository : TableStorageBase<NotificationBanner>// INotificationBannerRepository
+    public class NotificationBannerRepository : TableStorageBase<NotificationBanner> // INotificationBannerRepository
     {
+        private const string TopTwoNotificationBannersCacheKey = "topTwoCurrentNotificationBanners";
+
         public NotificationBannerRepository()
             : base("DataConnectionString")
         {
         }
 
-        public async Task CreateAsync(NotificationBanner entity) => await Table.ExecuteAsync(TableOperation.Insert(entity));
+        public async Task CreateAsync(NotificationBanner entity)
+        {
+            await Table.ExecuteAsync(TableOperation.Insert(entity));
+        }
 
         public async Task CreateAsync(params NotificationBanner[] entities)
         {
             /*
-             * A batch operation is a collection of table operations which are executed by the Storage Service REST API as a 
-             * single atomic operation, by invoking an Entity Group Transaction. A batch operation may contain up to 100 individual table operations, 
-             * with the requirement that each operation entity must have same partition key. A batch with a retrieve operation cannot contain 
+             * A batch operation is a collection of table operations which are executed by the Storage Service REST API as a
+             * single atomic operation, by invoking an Entity Group Transaction. A batch operation may contain up to 100 individual table operations,
+             * with the requirement that each operation entity must have same partition key. A batch with a retrieve operation cannot contain
              * any other operations. Note that the total payload of a batch operation is limited to 4MB.
              */
             var partitionKeys = entities.Select(x => x.PartitionKey).Distinct();
@@ -40,35 +48,66 @@ namespace Edubase.Data.Repositories
             }
         }
 
-        public async Task CreateAsync(IEnumerable<NotificationBanner> entities) => await CreateAsync(entities.ToArray());
-
-        public Page<NotificationBanner> GetAll(int take, TableContinuationToken skip = null, bool visibleOnly = true, eNotificationBannerPartition partitionKey = eNotificationBannerPartition.Current)
+        public async Task CreateAsync(IEnumerable<NotificationBanner> entities)
         {
-            var query = Table.CreateQuery<NotificationBanner>().Where(x => x.PartitionKey == partitionKey.ToString()).AsQueryable();
-            if (visibleOnly)
-            {
-                query = query.Where(x => x.Start <= DateTime.Now && x.End >= DateTime.Now);
-            }
-            query = query.Take(take);
-            var results = Table.ExecuteQuerySegmentedAsync(query.AsTableQuery(), skip).Result;
-            return new Page<NotificationBanner>(results, results.ContinuationToken);
+            await CreateAsync(entities.ToArray());
         }
 
-        public async Task<Page<NotificationBanner>> GetAllAsync(int take, TableContinuationToken skip = null, bool excludeExpired = false, eNotificationBannerPartition partitionKey = eNotificationBannerPartition.Current)
+        public IEnumerable<NotificationBanner> GetTopTwoCurrentNotificationBanners()
         {
-            var query = Table.CreateQuery<NotificationBanner>().Where(x => x.PartitionKey == partitionKey.ToString()).AsQueryable();
+            var cache = MemoryCache.Default;
+            var result = cache[TopTwoNotificationBannersCacheKey] as IEnumerable<NotificationBanner>;
+            return result ?? RetrieveAndUpdateTopTwoNotificationBannersFromCache();
+        }
+
+        private IEnumerable<NotificationBanner> RetrieveAndUpdateTopTwoNotificationBannersFromCache()
+        {
+            if (!int.TryParse(ConfigurationManager.AppSettings["NotificationBannerCacheExpirationInMinutes"],
+                    out var cacheExpirationInMinutes))
+            {
+                cacheExpirationInMinutes = 5;
+            }
+
+            var cache = MemoryCache.Default;
+
+            var query =
+                Table.CreateQuery<NotificationBanner>()
+                    .Where(x => x.PartitionKey == eNotificationBannerPartition.Current.ToString()
+                                && x.Start <= DateTime.Now
+                                && x.End >= DateTime.Now)
+                    .Take(2).AsTableQuery();
+            var notificationBanners = Table.ExecuteQuery(query).ToList();
+            Console.WriteLine("current thread is background: " + Thread.CurrentThread.IsBackground);
+
+            //the cache only adds values if they don't exist - the method name doesn't make this clear.
+            cache.Add(TopTwoNotificationBannersCacheKey,
+                notificationBanners,
+                new DateTimeOffset(DateTime.Now.AddMinutes(cacheExpirationInMinutes)));
+
+            return notificationBanners;
+        }
+
+        public async Task<Page<NotificationBanner>> GetAllAsync(int take, TableContinuationToken skip = null,
+            bool excludeExpired = false,
+            eNotificationBannerPartition partitionKey = eNotificationBannerPartition.Current)
+        {
+            var query = Table.CreateQuery<NotificationBanner>().Where(x => x.PartitionKey == partitionKey.ToString())
+                .AsQueryable();
             if (excludeExpired)
             {
                 query = query.Where(x => x.End >= DateTime.Now);
             }
+
             query = query.Take(take);
             var results = await Table.ExecuteQuerySegmentedAsync(query.AsTableQuery(), skip);
             return new Page<NotificationBanner>(results, results.ContinuationToken);
         }
 
-        public async Task<NotificationBanner> GetAsync(string id, eNotificationBannerPartition partitionKey = eNotificationBannerPartition.Current)
+        public async Task<NotificationBanner> GetAsync(string id,
+            eNotificationBannerPartition partitionKey = eNotificationBannerPartition.Current)
         {
-            var q = Table.CreateQuery<NotificationBanner>().Where(x => x.PartitionKey == partitionKey.ToString() && x.RowKey == id).AsTableQuery();
+            var q = Table.CreateQuery<NotificationBanner>()
+                .Where(x => x.PartitionKey == partitionKey.ToString() && x.RowKey == id).AsTableQuery();
             var results = await q.ExecuteSegmentedAsync(null);
             return results.FirstOrDefault();
         }
@@ -89,6 +128,7 @@ namespace Edubase.Data.Repositories
             {
                 item.RowKey = Guid.NewGuid().ToString("N").Substring(0, 8);
             }
+
             await CreateAsync(item);
 
 
@@ -113,6 +153,5 @@ namespace Edubase.Data.Repositories
             item.Version++;
             await Table.ExecuteAsync(TableOperation.Replace(item));
         }
-
     }
 }
