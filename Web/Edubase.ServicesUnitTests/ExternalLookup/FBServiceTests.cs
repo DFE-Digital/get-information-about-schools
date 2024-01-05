02e1b81c8a4ee372fb5b24f65c7bc28a9410ecd4
Barry GIBNEY <Barry.GIBNEY@education.gov.uk>
2024-01-05 08:13:17 +0000

    test renamed to example - as suggested

--- Web/Edubase.ServicesUnitTests/ExternalLookup/FBServiceTests.cs
+++ Web/Edubase.ServicesUnitTests/ExternalLookup/FBServiceTests.cs
@@ -12,14 +12,14 @@ namespace Edubase.Services.ExternalLookup.UnitTests
         public FBServiceTests(ITestOutputHelper output)
         {
             _output = output;
-            ConfigurationManager.AppSettings["FinancialBenchmarkingApiURL"] = @"http://test/";
-            ConfigurationManager.AppSettings["FinancialBenchmarkingURL"] = @"http://test/";
+            ConfigurationManager.AppSettings["FinancialBenchmarkingApiURL"] = @"http://example/";
+            ConfigurationManager.AppSettings["FinancialBenchmarkingURL"] = @"http://example/";
         }
 
         [Theory]
-        [InlineData(1, FbType.School, "http://test/school/detail?urn=1", "PublicURL_ReturnsCorrectUrl_School")]
-        [InlineData(1, FbType.Federation, "http://test/federation?fuid=1", "PublicURL_ReturnsCorrectUrl_Federation")]
-        [InlineData(1, FbType.Trust, "http://test/Trust?companyNo=1", "PublicURL_ReturnsCorrectUrl_Trust")]
+        [InlineData(1, FbType.School, "http://example/school/detail?urn=1", "PublicURL_ReturnsCorrectUrl_School")]
+        [InlineData(1, FbType.Federation, "http://example/federation?fuid=1", "PublicURL_ReturnsCorrectUrl_Federation")]
+        [InlineData(1, FbType.Trust, "http://example/Trust?companyNo=1", "PublicURL_ReturnsCorrectUrl_Trust")]
         public void PublicURL_ReturnsCorrectUrl(int? lookupId, FbType lookupType, string expectedUrl, string testName)
         {
             _output.WriteLine(testName);
@@ -29,9 +29,9 @@ namespace Edubase.Services.ExternalLookup.UnitTests
         }
 
         [Theory]
-        [InlineData(1, FbType.School, "http://test/api/schoolstatus/1", "ApiUrl_ReturnsCorrectUrl_School")]
-        [InlineData(1, FbType.Federation, "http://test/api/federationstatus/1", "ApiUrl_ReturnsCorrectUrl_Federation")]
-        [InlineData(1, FbType.Trust, "http://test/api/truststatus/1", "ApiUrl_ReturnsCorrectUrl_Trust")]
+        [InlineData(1, FbType.School, "http://example/api/schoolstatus/1", "ApiUrl_ReturnsCorrectUrl_School")]
+        [InlineData(1, FbType.Federation, "http://example/api/federationstatus/1", "ApiUrl_ReturnsCorrectUrl_Federation")]
+        [InlineData(1, FbType.Trust, "http://example/api/truststatus/1", "ApiUrl_ReturnsCorrectUrl_Trust")]
         public void ApiUrl_ReturnsCorrectUrl(int? lookupId, FbType lookupType, string expectedUrl, string testName)
         {
             _output.WriteLine(testName);
