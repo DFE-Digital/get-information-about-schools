9a81c9ffd7d98ae71b272edc30ce38baec910369
Barry GIBNEY <Barry.GIBNEY@education.gov.uk>
2024-10-02 09:38:09 +0100

    Changes made with InsertOrReplace removed.

--- Web/Edubase.Web.UI/Global.asax.cs
+++ Web/Edubase.Web.UI/Global.asax.cs
@@ -11,7 +11,6 @@ using System.Net.Security;
 using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
