d457b858bb15aba3b506cac70adebf7b82956937
Roger Howell <roger.howell@education.gov.uk>
2024-10-11 10:54:02 +0100

    Additional test cases

--- Web/Edubase.Web.UIUnitTests/Areas/Governors/Models/SelectSharedGovernorViewModelValidatorTests.cs
+++ Web/Edubase.Web.UIUnitTests/Areas/Governors/Models/SelectSharedGovernorViewModelValidatorTests.cs
@@ -10,16 +10,23 @@ namespace Edubase.Web.UIUnitTests.Areas.Governors.Models
 {
     public class SelectSharedGovernorViewModelValidatorTests
     {
-        private readonly SelectSharedGovernorViewModelValidator _validator = new SelectSharedGovernorViewModelValidator();
+        private readonly SelectSharedGovernorViewModelValidator _validator =
+            new SelectSharedGovernorViewModelValidator();
 
         private const eLookupGovernorRole ArbitrarySinglePersonRole = eLookupGovernorRole.ChairOfGovernors;
         private const eLookupGovernorRole ArbitraryMultiPersonRole = eLookupGovernorRole.Governor;
 
-        private readonly DateTimeViewModel _arbitraryValidStartDate = new DateTimeViewModel { Day = 1, Month = 1, Year = 2020 };
-        private readonly DateTimeViewModel _arbitraryValidEndDate = new DateTimeViewModel { Day = 1, Month = 1, Year = 2055 };
+        private readonly DateTimeViewModel _arbitraryValidStartDate =
+            new DateTimeViewModel { Day = 1, Month = 1, Year = 2020 };
 
-        private readonly DateTimeViewModel _arbitraryInvalidStartDate = new DateTimeViewModel { Day = 50, Month = 20, Year = null };
-        private readonly DateTimeViewModel _arbitraryInvalidEndDate = new DateTimeViewModel { Day = 50, Month = 20, Year = null };
+        private readonly DateTimeViewModel _arbitraryValidEndDate =
+            new DateTimeViewModel { Day = 1, Month = 1, Year = 2055 };
+
+        private readonly DateTimeViewModel _arbitraryInvalidStartDate =
+            new DateTimeViewModel { Day = 50, Month = 20, Year = null };
+
+        private readonly DateTimeViewModel _arbitraryInvalidEndDate =
+            new DateTimeViewModel { Day = 50, Month = 20, Year = null };
 
         [Fact]
         public void SinglePersonRole_SelectedGovernorId_WhenNull_ThenError()
@@ -163,9 +170,7 @@ namespace Edubase.Web.UIUnitTests.Areas.Governors.Models
                 {
                     new SharedGovernorViewModel
                     {
-                        Id = 1,
-                        Selected = false,
-                        AppointmentStartDate = _arbitraryInvalidStartDate // Invalid entry
+                        Id = 1, Selected = false, AppointmentStartDate = _arbitraryInvalidStartDate // Invalid entry
                     }
                 },
                 Role = ArbitraryMultiPersonRole,
@@ -237,8 +242,56 @@ namespace Edubase.Web.UIUnitTests.Areas.Governors.Models
                 SelectedGovernorId = "2",
                 Governors = new List<SharedGovernorViewModel>
                 {
-                    new SharedGovernorViewModel { Id = 1, Selected = false, AppointmentStartDate = _arbitraryValidStartDate, AppointmentEndDate = _arbitraryValidEndDate },
-                    new SharedGovernorViewModel { Id = 2, Selected = true, AppointmentStartDate = _arbitraryValidStartDate, AppointmentEndDate = _arbitraryValidEndDate },
+                    new SharedGovernorViewModel
+                    {
+                        Id = 1,
+                        Selected = false,
+                        AppointmentStartDate = _arbitraryValidStartDate,
+                        AppointmentEndDate = _arbitraryValidEndDate
+                    },
+                    new SharedGovernorViewModel
+                    {
+                        Id = 2,
+                        Selected = true,
+                        AppointmentStartDate = _arbitraryValidStartDate,
+                        AppointmentEndDate = _arbitraryValidEndDate
+                    },
+                },
+                Role = ArbitrarySinglePersonRole,
+            };
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.SelectedGovernorId);
+        }
+
+        [Fact]
+        public void
+            SinglePersonRole_SelectedGovernorId_WhenMultipleGovernorsAndOneSelected__And_MismatchWithSelected__NoError()
+        {
+            // Arrange
+            var model = new SelectSharedGovernorViewModel
+            {
+                SelectedGovernorId =
+                    "1", // Note for single role, only this is used and the `Selected` property is disregarded.
+                Governors = new List<SharedGovernorViewModel>
+                {
+                    new SharedGovernorViewModel
+                    {
+                        Id = 1,
+                        Selected = false,
+                        AppointmentStartDate = _arbitraryValidStartDate,
+                        AppointmentEndDate = _arbitraryValidEndDate
+                    },
+                    new SharedGovernorViewModel
+                    {
+                        Id = 2,
+                        Selected = true,
+                        AppointmentStartDate = _arbitraryValidStartDate,
+                        AppointmentEndDate = _arbitraryValidEndDate
+                    },
                 },
                 Role = ArbitrarySinglePersonRole,
             };
@@ -259,8 +312,20 @@ namespace Edubase.Web.UIUnitTests.Areas.Governors.Models
                 SelectedGovernorId = null,
                 Governors = new List<SharedGovernorViewModel>
                 {
-                    new SharedGovernorViewModel { Id = 1, Selected = true, AppointmentStartDate = _arbitraryValidStartDate, AppointmentEndDate = _arbitraryValidEndDate },
-                    new SharedGovernorViewModel { Id = 2, Selected = false, AppointmentStartDate = _arbitraryValidStartDate, AppointmentEndDate = _arbitraryValidEndDate },
+                    new SharedGovernorViewModel
+                    {
+                        Id = 1,
+                        Selected = true,
+                        AppointmentStartDate = _arbitraryValidStartDate,
+                        AppointmentEndDate = _arbitraryValidEndDate
+                    },
+                    new SharedGovernorViewModel
+                    {
+                        Id = 2,
+                        Selected = false,
+                        AppointmentStartDate = _arbitraryValidStartDate,
+                        AppointmentEndDate = _arbitraryValidEndDate
+                    },
                 },
                 Role = ArbitraryMultiPersonRole,
             };
@@ -281,8 +346,20 @@ namespace Edubase.Web.UIUnitTests.Areas.Governors.Models
                 SelectedGovernorId = null,
                 Governors = new List<SharedGovernorViewModel>
                 {
-                    new SharedGovernorViewModel { Id = 1, Selected = true, AppointmentStartDate = _arbitraryValidStartDate, AppointmentEndDate = _arbitraryValidEndDate },
-                    new SharedGovernorViewModel { Id = 2, Selected = true, AppointmentStartDate = _arbitraryValidStartDate, AppointmentEndDate = _arbitraryValidEndDate },
+                    new SharedGovernorViewModel
+                    {
+                        Id = 1,
+                        Selected = true,
+                        AppointmentStartDate = _arbitraryValidStartDate,
+                        AppointmentEndDate = _arbitraryValidEndDate
+                    },
+                    new SharedGovernorViewModel
+                    {
+                        Id = 2,
+                        Selected = true,
+                        AppointmentStartDate = _arbitraryValidStartDate,
+                        AppointmentEndDate = _arbitraryValidEndDate
+                    },
                 },
                 Role = ArbitraryMultiPersonRole,
             };
@@ -293,5 +370,234 @@ namespace Edubase.Web.UIUnitTests.Areas.Governors.Models
             // Assert
             result.ShouldNotHaveValidationErrorFor(x => x.Governors);
         }
+
+        [Fact]
+        public void SinglePersonRole_SelectedGovernorId_WhenSelectedGovernorHasInvalidDates_ThenError()
+        {
+            // Arrange
+            var model = new SelectSharedGovernorViewModel
+            {
+                SelectedGovernorId = "1",
+                Governors = new List<SharedGovernorViewModel>
+                {
+                    new SharedGovernorViewModel
+                    {
+                        Id = 1,
+                        FullName = "Governor Name",
+                        Selected = true,
+                        AppointmentStartDate = _arbitraryInvalidStartDate,
+                        AppointmentEndDate = _arbitraryInvalidEndDate
+                    }
+                },
+                Role = ArbitrarySinglePersonRole,
+            };
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor($"Governors[0].{nameof(SharedGovernorViewModel.AppointmentStartDate)}")
+                .WithErrorMessage("Enter a valid appointment start date for Governor Name (1)");
+
+            result.ShouldHaveValidationErrorFor($"Governors[0].{nameof(SharedGovernorViewModel.AppointmentEndDate)}")
+                .WithErrorMessage("Enter a valid appointment end date for Governor Name (1)");
+        }
+
+        [Fact]
+        public void SinglePersonRole_SelectedGovernorId_WhenMultipleGovernorsAndSelectedGovernorHasInvalidDates_ThenError()
+        {
+            // Arrange
+            var model = new SelectSharedGovernorViewModel
+            {
+                SelectedGovernorId = "2",
+                Governors = new List<SharedGovernorViewModel>
+                {
+                    new SharedGovernorViewModel
+                    {
+                        Id = 1,
+                        FullName = "Governor Name",
+                        AppointmentStartDate = _arbitraryValidStartDate,
+                        AppointmentEndDate = _arbitraryValidEndDate
+                    },
+                    new SharedGovernorViewModel
+                    {
+                        Id = 2,
+                        FullName = "Governor Name 2",
+                        AppointmentStartDate = _arbitraryInvalidStartDate,
+                        AppointmentEndDate = _arbitraryInvalidEndDate
+                    },
+                },
+                Role = ArbitrarySinglePersonRole,
+            };
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor($"Governors[1].{nameof(SharedGovernorViewModel.AppointmentStartDate)}")
+                .WithErrorMessage("Enter a valid appointment start date for Governor Name 2 (2)");
+
+            result.ShouldHaveValidationErrorFor($"Governors[1].{nameof(SharedGovernorViewModel.AppointmentEndDate)}")
+                .WithErrorMessage("Enter a valid appointment end date for Governor Name 2 (2)");
+        }
+
+        [Fact]
+        public void MultiPersonRole_Governors_WhenSelectedGovernorHasInvalidDates_ThenError()
+        {
+            // Arrange
+            var model = new SelectSharedGovernorViewModel
+            {
+                SelectedGovernorId = null,
+                Governors = new List<SharedGovernorViewModel>
+                {
+                    new SharedGovernorViewModel
+                    {
+                        Id = 1,
+                        FullName = "Governor Name",
+                        Selected = true,
+                        AppointmentStartDate = _arbitraryInvalidStartDate,
+                        AppointmentEndDate = _arbitraryInvalidEndDate
+                    }
+                },
+                Role = ArbitraryMultiPersonRole,
+            };
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor($"Governors[0].{nameof(SharedGovernorViewModel.AppointmentStartDate)}")
+                .WithErrorMessage("Enter a valid appointment start date for Governor Name (1)");
+
+            result.ShouldHaveValidationErrorFor($"Governors[0].{nameof(SharedGovernorViewModel.AppointmentEndDate)}")
+                .WithErrorMessage("Enter a valid appointment end date for Governor Name (1)");
+        }
+
+        [Fact]
+        public void MultiPersonRole_Governors_WhenMultipleGovernorsAndSelectedGovernorHasInvalidDates_ThenError()
+        {
+            // Arrange
+            var model = new SelectSharedGovernorViewModel
+            {
+                SelectedGovernorId = null,
+                Governors = new List<SharedGovernorViewModel>
+                {
+                    new SharedGovernorViewModel
+                    {
+                        Id = 1,
+                        FullName = "Governor Name",
+                        Selected = false, // Selected matters for multi roles
+                        AppointmentStartDate = _arbitraryValidStartDate,
+                        AppointmentEndDate = _arbitraryValidEndDate
+                    },
+                    new SharedGovernorViewModel
+                    {
+                        Id = 2,
+                        FullName = "Governor Name 2",
+                        Selected = true, // Selected matters for multi roles
+                        AppointmentStartDate = _arbitraryInvalidStartDate,
+                        AppointmentEndDate = _arbitraryInvalidEndDate
+                    },
+                },
+                Role = ArbitraryMultiPersonRole,
+            };
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor($"Governors[1].{nameof(SharedGovernorViewModel.AppointmentStartDate)}")
+                .WithErrorMessage("Enter a valid appointment start date for Governor Name 2 (2)");
+
+            result.ShouldHaveValidationErrorFor($"Governors[1].{nameof(SharedGovernorViewModel.AppointmentEndDate)}")
+                .WithErrorMessage("Enter a valid appointment end date for Governor Name 2 (2)");
+        }
+
+        [Fact]
+        public void MultiPersonRole_Governors_WhenMultipleGovernorsAndMultipleSelectedAndOneHasInvalidDates_ThenError()
+        {
+            // Arrange
+            var model = new SelectSharedGovernorViewModel
+            {
+                SelectedGovernorId = null,
+                Governors = new List<SharedGovernorViewModel>
+                {
+                    new SharedGovernorViewModel
+                    {
+                        Id = 1,
+                        FullName = "Governor Name",
+                        Selected = true,
+                        AppointmentStartDate = _arbitraryValidStartDate,
+                        AppointmentEndDate = _arbitraryValidEndDate
+                    },
+                    new SharedGovernorViewModel
+                    {
+                        Id = 2,
+                        FullName = "Governor Name 2",
+                        Selected = true,
+                        AppointmentStartDate = _arbitraryInvalidStartDate,
+                        AppointmentEndDate = _arbitraryInvalidEndDate
+                    },
+                },
+                Role = ArbitraryMultiPersonRole,
+            };
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor($"Governors[1].{nameof(SharedGovernorViewModel.AppointmentStartDate)}")
+                .WithErrorMessage("Enter a valid appointment start date for Governor Name 2 (2)");
+
+            result.ShouldHaveValidationErrorFor($"Governors[1].{nameof(SharedGovernorViewModel.AppointmentEndDate)}")
+                .WithErrorMessage("Enter a valid appointment end date for Governor Name 2 (2)");
+        }
+
+        [Fact]
+        public void MultiPersonRole_Governors_WhenMultipleGovernorsAndMultipleSelectedAndAllHaveInvalidDates_ThenError()
+        {
+            // Arrange
+            var model = new SelectSharedGovernorViewModel
+            {
+                SelectedGovernorId = null,
+                Governors = new List<SharedGovernorViewModel>
+                {
+                    new SharedGovernorViewModel
+                    {
+                        Id = 1,
+                        FullName = "Governor Name",
+                        Selected = true,
+                        AppointmentStartDate = _arbitraryInvalidStartDate,
+                        AppointmentEndDate = _arbitraryInvalidEndDate
+                    },
+                    new SharedGovernorViewModel
+                    {
+                        Id = 2,
+                        FullName = "Governor Name 2",
+                        Selected = true,
+                        AppointmentStartDate = _arbitraryInvalidStartDate,
+                        AppointmentEndDate = _arbitraryInvalidEndDate
+                    },
+                },
+                Role = ArbitraryMultiPersonRole,
+            };
+
+            // Act
+            var result = _validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor($"Governors[0].{nameof(SharedGovernorViewModel.AppointmentStartDate)}")
+                .WithErrorMessage("Enter a valid appointment start date for Governor Name (1)");
+
+            result.ShouldHaveValidationErrorFor($"Governors[0].{nameof(SharedGovernorViewModel.AppointmentEndDate)}")
+                .WithErrorMessage("Enter a valid appointment end date for Governor Name (1)");
+
+            result.ShouldHaveValidationErrorFor($"Governors[1].{nameof(SharedGovernorViewModel.AppointmentStartDate)}")
+                .WithErrorMessage("Enter a valid appointment start date for Governor Name 2 (2)");
+
+            result.ShouldHaveValidationErrorFor($"Governors[1].{nameof(SharedGovernorViewModel.AppointmentEndDate)}")
+                .WithErrorMessage("Enter a valid appointment end date for Governor Name 2 (2)");
+        }
+
     }
 }
