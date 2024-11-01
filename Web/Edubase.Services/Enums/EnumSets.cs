232fbbc07bf5993aa48120775825736867852f69
RogerHowellDfE <96429508+RogerHowellDfE@users.noreply.github.com>
2024-11-01 14:40:42 +0000

    231733  allow multiple `shared governance professional - group` (#601)
    #231733

--- Web/Edubase.Services/Enums/EnumSets.cs
+++ Web/Edubase.Services/Enums/EnumSets.cs
@@ -86,7 +86,49 @@ namespace Edubase.Services.Enums
             GR.GovernanceProfessionalToASecureSat,
         };
 
-        public static IEnumerable<int> GovernanceProfessionalRoles = eGovernanceProfessionalRoles.Cast<int>();
+        public static IEnumerable<int> GovernanceProfessionalRoles { get; } = eGovernanceProfessionalRoles.Cast<int>();
+
+
+
+        /// <summary>
+        /// Every governance professional role, combined with every type of governance professional.
+        /// </summary>
+        public static IEnumerable<GR[]> PairwiseGovernanceProfessionalRoles =>
+        (
+            from preExistingRole in EnumSets.eGovernanceProfessionalRoles
+            from newRole in EnumSets.eGovernanceProfessionalRoles
+            select new GR[] { preExistingRole, newRole }
+        );
+
+
+        /// <summary>
+        /// Existing role, new role
+        /// </summary>
+        public static IEnumerable<GR[]> PermittedGovernanceProfessionalCombinations => new List<GR[]>
+        {
+            // - #198772 / #193913 : MAT can have "one-each" of "Shared governance professional - group" and "Governance professional to a multi-academy trust (MAT)"
+            // - #231733: Can now also have many "Shared governance professional - group" roles, not just "one of each"
+            new [] {GR.Group_SharedGovernanceProfessional, GR.GovernanceProfessionalToAMat},
+            new [] {GR.GovernanceProfessionalToAMat, GR.Group_SharedGovernanceProfessional},
+
+            // - #198772 / #197361 : Establishment within a SAT can have "one-each" of "Governance professional to an individual academy or free school" and "Governance professional for single-academy trust (SAT)"
+            new [] {GR.GovernanceProfessionalToAnIndividualAcademyOrFreeSchool, GR.GovernanceProfessionalToASat},
+            new [] {GR.GovernanceProfessionalToASat, GR.GovernanceProfessionalToAnIndividualAcademyOrFreeSchool},
+
+            // - #198239: System should allow adding a Governance professional to a federation if a record for Governance professional to a local authority maintained is already recorded.
+            new [] {GR.GovernanceProfessionalToAFederation, GR.GovernanceProfessionalToALocalAuthorityMaintainedSchool},
+            new [] {GR.GovernanceProfessionalToALocalAuthorityMaintainedSchool, GR.GovernanceProfessionalToAFederation},
+
+            // - #231733: Can now have many "Shared governance professional - group" roles, not just "one of each"
+            new [] {GR.Group_SharedGovernanceProfessional, GR.Group_SharedGovernanceProfessional},
+        };
+
+        /// All combinations of governance professional roles are forbidden, minus those which are explicitly permitted in PermittedGovernanceProfessionalCombinations
+        /// First entry is the pre-existing role, and second entry is the proposed new role.
+        public static IEnumerable<GR[]> ForbiddenCombinationsOfGovernanceProfessionalRoles => PairwiseGovernanceProfessionalRoles
+            .Where(allPairsPair => !PermittedGovernanceProfessionalCombinations.Any(innerPair =>
+                allPairsPair[0].Equals(innerPair[0])
+                && allPairsPair[1].Equals(innerPair[1])));
 
 
         public static IEnumerable<GR> eChairOfLocalGoverningBodyRoles { get; } = new[]
@@ -96,6 +138,6 @@ namespace Edubase.Services.Enums
             GR.ChairOfLocalGoverningBody
         };
 
-        public static IEnumerable<int> ChairOfLocalGoverningBodyRoles = eChairOfLocalGoverningBodyRoles.Cast<int>();
+        public static IEnumerable<int> ChairOfLocalGoverningBodyRoles { get; } = eChairOfLocalGoverningBodyRoles.Cast<int>();
     }
 }
