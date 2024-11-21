bd8661ecfcfe29767e00578d13f3d24c1966fbc5
Barry GIBNEY <Barry.GIBNEY@education.gov.uk>
2024-11-21 15:20:59 +0000

    Capture Polly.Timeout.TimeoutException

--- Web/Edubase.Services/IntegrationEndPoints/PollyUtil.cs
+++ Web/Edubase.Services/IntegrationEndPoints/PollyUtil.cs
@@ -27,7 +27,10 @@ namespace Edubase.Services.IntegrationEndPoints
             var retryPolicy = Policy<HttpResponseMessage>
                 .Handle<HttpRequestException>()
                 .Or<TaskCanceledException>()
-                .WaitAndRetryAsync(retryIntervals);
+                .WaitAndRetryAsync(retryIntervals, onRetry: (outcome, TimeSpan, retryAttempt, context) =>
+                {
+                    Console.WriteLine($"retry attempt {retryAttempt} due to: {outcome.Exception?.Message}");
+                });
 
             var timeoutPolicy = CreateTimeoutPolicy(settingsKey);
 
@@ -41,7 +44,17 @@ namespace Edubase.Services.IntegrationEndPoints
                 timeoutSettings = 10;
             }
 
-            return Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(timeoutSettings));
+            return Policy.TimeoutAsync<HttpResponseMessage>(
+                TimeSpan.FromSeconds(timeoutSettings),
+                onTimeoutAsync: async (context, timeout, task, exception) =>
+                {
+                    Console.WriteLine($"timed out after {timeout.TotalSeconds} seconds - Exception {exception.Message}");
+                    if (exception is Polly.Timeout.TimeoutRejectedException)
+                    {
+                        Console.WriteLine("Polly.Timeout.Exception detected");
+                    }
+                    await Task.CompletedTask;
+                });
         }
 
         /// <summary>
