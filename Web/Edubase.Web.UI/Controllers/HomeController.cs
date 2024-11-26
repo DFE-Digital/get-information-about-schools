6b4277eea6bc98fc9d71205b8f87af5095bbeca4
Barry GIBNEY <Barry.GIBNEY@education.gov.uk>
2024-11-26 14:55:42 +0000

    Error accessing parent directory

--- Web/Edubase.Web.UI/Controllers/HomeController.cs
+++ Web/Edubase.Web.UI/Controllers/HomeController.cs
@@ -95,7 +95,7 @@ namespace Edubase.Web.UI.Controllers
             var returnTo = Request.Form["OriginatingPage"];
             Response.Cookies.Set(new HttpCookie(UserPrefsCookieName, acceptAnalyticsCookies.ToString()) { Expires = DateTime.Today.AddDays(28), SameSite = SameSiteMode.Lax, Domain = cookieDomain });
             TempData["CookiesPrefsSaved"] = acceptAnalyticsCookies;
-            if (returnTo != null)
+            if (!string.IsNullOrEmpty(returnTo) && urlHelper.IsLocalUrl(returnTo))
             {
                 return Redirect(returnTo);
             }
