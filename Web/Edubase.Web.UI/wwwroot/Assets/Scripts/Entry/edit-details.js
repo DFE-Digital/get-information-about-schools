34c076f4fae00e77eec081cae6a1d79452be8f9e
anthony9853 <113428310+anthony9853@users.noreply.github.com>
2025-12-03 16:59:53 +0000

    fixing npm problems

--- Web/Edubase.Web.UI/wwwroot/Assets/Scripts/Entry/edit-details.js
+++
@@ -1,5 +0,0 @@
-import GiasAttachUnload from "../GiasModules/GiasModals/GiasAttachUnload";
-import typeToPhase from "../GiasModules/GiasEditTypePhases";
-new GiasAttachUnload();
-
-typeToPhase.bindEvents();
--- Web/Edubase.Web.UI/wwwroot/Assets/Scripts/GiasHelpers/supportsHistory.js
+++
@@ -1,5 +0,0 @@
-function supportsHistory() {
-  return window.history && window.history.pushState && window.history.replaceState;
-}
-
-export default supportsHistory;
