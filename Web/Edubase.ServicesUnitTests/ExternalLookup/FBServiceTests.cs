6103d4f47ab2c67ca96c4a2c9777212744e7fcc9
SKHURSHID <sajid.khurshid@education.gov.uk>
2026-03-26 14:09:31 +0000

    Reenable test with 1 piece of test data.

--- Web/Edubase.ServicesUnitTests/ExternalLookup/FBServiceTests.cs
+++ Web/Edubase.ServicesUnitTests/ExternalLookup/FBServiceTests.cs
@@ -18,17 +18,17 @@ namespace Edubase.Services.ExternalLookup.UnitTests
             ConfigurationManager.AppSettings["FBService_RetryIntervals"] = "1";
         }
 
-        //[Theory]
-        //[InlineData(1, FbType.School, "https://example.com/api/schoolstatus/1")]
+        [Theory]
+        [InlineData(1, FbType.School, "https://example.com/api/schoolstatus/1")]
         //[InlineData(1, FbType.Federation, "https://example.com/api/federationstatus/1")]
         //[InlineData(1, FbType.Trust, "https://example.com/api/truststatus/1")]
-        //public void ApiUrl_ReturnsCorrectUrl(int? lookupId, FbType lookupType, string expectedUrl)
-        //{
-        //    ConfigurationManager.AppSettings["FinancialBenchmarkingApiURL"] = "https://example.com/";
-        //    var client = new HttpClient();
-        //    var subject = new FBService(client);
-        //    var apiUrl = subject.ApiUrl(lookupId, lookupType);
-        //    Assert.Equal(expectedUrl, apiUrl);
-        //}
+        public void ApiUrl_ReturnsCorrectUrl(int? lookupId, FbType lookupType, string expectedUrl)
+        {
+            ConfigurationManager.AppSettings["FinancialBenchmarkingApiURL"] = "https://example.com/";
+            var client = new HttpClient();
+            var subject = new FBService(client);
+            var apiUrl = subject.ApiUrl(lookupId, lookupType);
+            Assert.Equal(expectedUrl, apiUrl);
+        }
     }
 }
