05257039c9fcd3151c7ca00d329d31e3571de04b
BarryGibney <barry.gibney@education.gov.uk>
2026-06-30 10:52:22 +0100

    BG/297414 Add Logging

--- Web/Edubase.Web.UI/Controllers/Api/SqlDataController.cs
+++ Web/Edubase.Web.UI/Controllers/Api/SqlDataController.cs
@@ -1,7 +1,9 @@
 using System.Configuration;
 using Microsoft.Data.SqlClient;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Trace = System.Diagnostics.Trace;
 
 namespace Edubase.Web.UI.Controllers.Api
 {
@@ -13,20 +15,38 @@ namespace Edubase.Web.UI.Controllers.Api
             var serverName = ConfigurationManager.AppSettings["SQLServer"];
             var databaseName = ConfigurationManager.AppSettings["SQLDatabase"];
 
+            Trace.TraceInformation($"[api/sql] SQL Server='{serverName}' (len={serverName?.Length}), " +
+                                   $"SQL Database='{databaseName}' (len={databaseName?.Length})");
+
             var connectionString =
-                $"Server=tcp:{serverName}.database.windows.net,1433;" +
+                $"Server=tcp:{serverName},1433;" +
                 $"database={databaseName};" +
                 "authentication=Active Directory Default;" + // leverages Managed Identity on App Service
                 "encrypt=True;TrustServerCertificate=False;";
 
-            using (var connection = new SqlConnection(connectionString))
+            try
             {
-                await connection.OpenAsync();
-                using (var command = new SqlCommand("SELECT GETUTCDATE();", connection))
+                using (var connection = new SqlConnection(connectionString))
                 {
-                    var result = await command.ExecuteScalarAsync();
-                    return Ok(new { serverDate = result, status = "Connected" });
+                    await connection.OpenAsync();
+                    using (var command = new SqlCommand("SELECT GETUTCDATE();", connection))
+                    {
+                        var result = await command.ExecuteScalarAsync();
+                        return Ok(new { serverDate = result, status = "Connected" });
+                    }
                 }
+            }
+            catch (SqlException ex)
+            {
+                Trace.TraceError($"[api/sql] connection failed: {ex.GetType().Name}: {ex.Message}");
+                return Content(HttpStatusCode.InternalServerError,
+                    new
+                    {
+                        sqlServer = serverName,
+                        sqlDatabase = databaseName,
+                        error = ex.GetType().Name,
+                        message = ex.Message
+                    });
             }
         }
     }
