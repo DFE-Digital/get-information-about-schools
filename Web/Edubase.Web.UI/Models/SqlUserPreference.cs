3b9e5ad4543d4e6e8bb4e45e7522b38fb5debc1a
BarryGibney <barry.gibney@education.gov.uk>
2026-07-15 14:03:08 +0100

    Mapping to the UserPreferences table

--- Web/Edubase.Web.UI/Models/SqlUserPreference.cs
+++ Web/Edubase.Web.UI/Models/SqlUserPreference.cs
@@ -1,7 +1,9 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Edubase.Web.UI.Models
 {
+    [Table("UserPreferences")]
     public class SqlUserPreference
     {
         public SqlUserPreference() { }
