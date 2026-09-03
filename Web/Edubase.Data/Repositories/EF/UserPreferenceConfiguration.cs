fea6b10af12cf520a0876e269f43fbb32aba29ff
Adam Parker <adam.parker@education.gov.uk>
2026-09-03 16:24:51 +0100

    Added comments

--- Web/Edubase.Data/Repositories/EF/UserPreferenceConfiguration.cs
+++ Web/Edubase.Data/Repositories/EF/UserPreferenceConfiguration.cs
@@ -3,6 +3,10 @@ using Edubase.Data.Entity;
 
 namespace Edubase.Data.Repositories.EF
 {
+    /// <summary>
+    ///  EF configuration class including ignoring table storage fields, so as to minimise change to existing model
+    ///  classes for migration to SQL storage. Can be revisited after migration
+    /// </summary>
     public class UserPreferenceConfiguration : EntityTypeConfiguration<UserPreference>
     {
         public UserPreferenceConfiguration()
